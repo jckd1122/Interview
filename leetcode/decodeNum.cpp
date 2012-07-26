f49fe3ff81dc08c228fdd46742867662f0454179
jckd1122 <wmiao1122@gmail.com>
2012-07-26 20:18:09 -0300

    Update leetcode/decodeNum.cpp

--- leetcode/decodeNum.cpp
+++ leetcode/decodeNum.cpp
@@ -17,10 +17,12 @@ public:
         //cout << v << endl;
         
         arr[0] = 1;
+        if(s[0] == '0' || s == "")
+            return 0;
 
         for(int i = 1; i < l; ++i){
-            istringstream is(s.substr(0,i+1));
-            is >> v;
+            istringstream is1(s.substr(0,i+1));
+            is1 >> v;
             if(v == 10 || v == 20)
                 arr[i] =  arr[i-1];
             else if(v >= 27 && v <=99 && v %10 == 0)
@@ -29,15 +31,21 @@ public:
                 arr[i] =  1;
             else if(v >= 11 && v <= 26)
                 arr[i] = 2;
-            else if(s[i] == 0)
+            else if(s[i] == '0' && s[i-1] == '0')
+                arr[i] = 0;
+            else if(s[i] == '0' && s[i-1] != '0' && s[i-1] <= '2')
                 arr[i] = arr[i-2];
+            else if(s[i] != '0' && s[i-1] == '0')
+                arr[i] = arr[i-1];
+            else if((s[i-1] >= '3' && s[i] != '0') || (s[i-1] == '2' && s[i] > '6'))
+                arr[i] = arr[i-1];
+            else if (s[i-1] >= '3' && s[i] == '0')
+                arr[i] = 0;
             else
                 arr[i] = arr[i-2]+arr[i-1];
         }
 
-        for(int i = 0; i < l;++i)
-            cout << arr[i] << endl;
-        cout << " " << endl;
+        
         return arr[l-1];
     }
 };
