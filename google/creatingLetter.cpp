14ff1c72f801b74328f880ac47b737474605b125
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-07-25 23:48:39 -0400

    delete

--- google/creatingLetter.cpp
+++ google/creatingLetter.cpp
@@ -7,27 +7,8 @@ using namespace std;
 
 
 //maximum A the procedure can generate
-int procedure(int N){
-    
-    int maxOutput = 0;
-    int output = 0;
-    for(int k = 1; k <= N;++k){
-        output = k;
-        for(int i = N-k; i > 0; i = i-3){
-            output *= 2;
-            if(i < 3){
-                output = output+i*output/2;
-                break;
-            }
-                
-        }
+//
 
-        if(output > maxOutput){
-            maxOutput = output;
-        }
-    }
-    return maxOutput;
-}
 
 int main(){
     cout << procedure(8) << endl;
