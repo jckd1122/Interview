455968d0af93540abfb60aa1b8806cd785556f03
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-07-26 20:00:49 -0400

    update

--- leetcode/decodeNum.cpp
+++ leetcode/decodeNum.cpp
@@ -20,22 +20,28 @@ public:
         for(int i = 1; i < l; ++i){
             istringstream is1(s.substr(0,i+1));
             is1 >> v;
-            
-            if(v >= 11 && v <= 26)
+            //if(v == 10 || v == 20)
+                arr[i] =  arr[i-1];
+            else if(v >= 27 && v <=99 && v %10 == 0)
+                arr[i] = 0;
+            else if(v >= 27 && v <= 99 && v %10 != 0)
+                arr[i] =  1;
+            else if(v >= 11 && v <= 26)
                 arr[i] = 2;
-            if(s[i] == '0' && s[i-1] == '0')
+            else if(s[i] == '0' && s[i-1] == '0')
                 arr[i] = 0;
             else if(s[i] == '0' && s[i-1] != '0' && s[i-1] <= '2')
                 arr[i] = arr[i-2];
-            else if (s[i] == '0' && s[i-1] >= '3')
-                arr[i] = 0;
             else if(s[i] != '0' && s[i-1] == '0')
                 arr[i] = arr[i-1];
             else if((s[i-1] >= '3' && s[i] != '0') || (s[i-1] == '2' && s[i] > '6'))
                 arr[i] = arr[i-1];
+            else if (s[i-1] >= '3' && s[i] == '0')
+                arr[i] = 0;
             else
                 arr[i] = arr[i-2]+arr[i-1];
         }
+
 
         
         return arr[l-1];
