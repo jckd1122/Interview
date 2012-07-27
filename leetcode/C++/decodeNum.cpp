959641cb13db81266920254cac596802ef6dbc39
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-07-26 23:06:53 -0400

    ch

---
+++ leetcode/C++/decodeNum.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
+//solve
+
+//need to simply
+class solution{
+public:
+    int cal(string s){
+        int v = 0;
+        int l = s.length();
+        int* arr = new int[20];
+        
+        arr[0] = 1;
+        if(s[0] == '0' || s == "")
+            return 0;
+
+        for(int i = 1; i < l; ++i){
+            istringstream is1(s.substr(0,i+1));
+            is1 >> v;
+            if(v == 10 || v == 20)
+                arr[i] =  arr[i-1];
+            else if(v >= 11 && v <= 26)
+                arr[i] = 2;
+            else if(s[i] == '0' && s[i-1] == '0')
+                arr[i] = 0;
+            else if(s[i] == '0' && s[i-1] != '0' && s[i-1] <= '2')
+                arr[i] = arr[i-2];
+            else if(s[i] != '0' && s[i-1] == '0')
+                arr[i] = arr[i-1];
+            else if((s[i-1] >= '3' && s[i] != '0') || (s[i-1] == '2' && s[i] > '6'))
+                arr[i] = arr[i-1];
+            else if (s[i-1] >= '3' && s[i] == '0')
+                arr[i] = 0;
+            else
+                arr[i] = arr[i-2]+arr[i-1];
+        }
+
+
+        
+        return arr[l-1];
+    }
+};
+
+
+int main(){
+    solution sol;
+    cout << sol.cal("112") << endl;
+
+}
