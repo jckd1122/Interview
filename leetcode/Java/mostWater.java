38a6a1296ea8541d594242489cfb2e3ed905b09d
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-07-26 23:31:25 -0400

    update

---
+++ leetcode/Java/mostWater.java
@@ -0,0 +1,26 @@
+
+
+
+public class mostWater{
+	
+	
+	public static int  maxArea(int[] height){
+		int length = 0;
+		int width = 0;
+		int area = 0;
+		int areaMax = 0;
+		for(int i = 0; i < height.length;++i){
+			for(int j = i+1; j < height.length;++j){
+				length = j-1;
+				width = (height[j] > hight[i])? height[i]:height[j];
+				area = length*width;
+				if(area > areaMax)
+					areaMax = area;
+				
+			}
+		}
+		return areaMax;
+		
+		
+	}
+}
