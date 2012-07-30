381f79398c2ecd772747a098f413756172038fa3
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-07-29 20:34:11 -0400

    update

--- leetcode/Java/mostWater.java
+++ leetcode/Java/mostWater.java
@@ -3,7 +3,7 @@
 public class mostWater{
 	
 	
-	public static int  maxArea(int[] height){
+	public static int  maxArea1(int[] height){
 		int length = 0;
 		int width = 0;
 		int area = 0;
@@ -21,5 +21,38 @@ public class mostWater{
 		return areaMax;
 		
 		
+	}
+	
+	public static int  maxArea2(int[] height){
+		int length = 0;
+		int width = 0;
+		int area = 0;
+		int areaMax = (height.length-1)*Math.min(height[l], height[r]); 
+		int l = 0;
+		int r = height.length-1;
+		while(l < r){
+			if(height[l] <= height[r]){
+				width = (height[l+1] > height[r])? height[l+1]:height[r];
+				area = (r-l-1)*width;
+				areaMax = Math.max(areaMax, area);
+				l++;
+			}
+			else{
+				width = (height[l] > height[r-1])? height[r-1]:height[l];
+				area = (r-l-1)*width;
+				areaMax = Math.max(areaMax, area);
+				r--;
+			}
+		}
+		return areaMax;
+	}
+	
+	
+	public static int maxArea3(int[] height){
+		int[] arr = new int[height.length];
+		//hashtable?
+		for(int i = 0; i < height.length;++i){
+			
+		}
 	}
 }
