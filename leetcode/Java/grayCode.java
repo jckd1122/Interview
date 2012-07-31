76c7626969624cc769e704f18635ce90602218c4
jckd1122 <wmiao1122@gmail.com>
2012-07-31 12:22:21 -0300

    Update leetcode/Java/grayCode.java

--- leetcode/Java/grayCode.java
+++ leetcode/Java/grayCode.java
@@ -1,16 +1,15 @@
-
 import java.lang.Math;
 
 public class grayCode{
     public static ArrayList<Integer> code(int n){
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(0);
-        list.add(1);
         for(int i = 2; i <= n;++i){
             int index = list.size()-1;
             for(int j = 1; j <= Math.pow(2.0,(double)i-1);++j){
                 int num = list.get(index);
-                list.add(num+Math.pow(2.0,(double)i-1));
+                double newNum = num+Math.pow(2.0,(double)i-1);
+                list.add((int)newNum);
                 index--;
             }
         }
