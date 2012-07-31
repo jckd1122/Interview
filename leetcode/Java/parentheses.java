26a515be42f3be84b7ac09d246b6cfff89a7b92b
jckd1122 <wmiao1122@gmail.com>
2012-07-31 15:55:07 -0300

    Update leetcode/Java/parentheses.java

--- leetcode/Java/parentheses.java
+++ leetcode/Java/parentheses.java
@@ -1,7 +1,15 @@
+import java.util.*;
 
-public class parentheses{
+public class Solution {
+    public ArrayList<String> generateParenthesis(int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<String> list = new ArrayList<String>();
+        return generateHelper(n,list);
+    }
     
-    public static ArrayList<String generateHelper(int n,ArrayList<String> parentSet){
+    
+    public ArrayList<String> generateHelper(int n,ArrayList<String> parentSet){
         if(n == 1){
             parentSet.add("()");
             return parentSet;
@@ -9,11 +17,11 @@ public class parentheses{
         else{
             for(int i = 0; i < parentSet.size();++i){
                 String ele = parentSet.get(i);
-                if(!parentSet.contains(ele+"()")
+                if(!parentSet.contains(ele+"()"))
                     parentSet.add(ele+"()");
-                if(!parentSet.contains("("+ele+")")
+                if(!parentSet.contains("("+ele+")"))
                     parentSet.add("("+ele+")");
-                if(!parentSet.contains("()"+ele)
+                if(!parentSet.contains("()"+ele))
                     parentSet.add("()"+ele);
             }
             return parentSet;
@@ -23,12 +31,4 @@ public class parentheses{
 
 
     }
-    public static ArrayList<String> generate(int n){
-        ArrayList<String> list = new ArrayList<String>();
-        return generateHelper(n,list);
-            
-    }
-
-
-
 }
