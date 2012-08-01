95d95d8a12ea3e79da258a44172dbc2dc502fb8c
jckd1122 <wmiao1122@gmail.com>
2012-08-01 13:04:54 -0300

    Update leetcode/Java/lastWord.java

--- leetcode/Java/lastWord.java
+++ leetcode/Java/lastWord.java
@@ -1,14 +1,16 @@
 
-
 public class lastWord{
 
     public int lengthOfLastWord(String s){
         int n = s.length-1;
+        int pos = 0;
         for(int i = s; i>= 0;--i){
-            if(s.charAt(i) == ' ')
-                return n-i;
+            if(s.charAt(i) == ' '){
+                pos = n-i;
+                break;
+            }
         }
-
+        return pos;
 
     }
 
