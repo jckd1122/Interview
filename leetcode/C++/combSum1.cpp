14dd33f3044f48b6e8fc1edb4b7618f17bc8a0b7
wenqiang <wmiao1122@gmail.com>
2012-08-02 11:27:38 -0400

    add

---
+++ leetcode/C++/combSum1.cpp
@@ -0,0 +1,28 @@
+class Solution{
+public:
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        vector<vector<int> > list;
+        vector<int> ele;
+        combHelper(0,0,target,candidates,ele,list);
+        return list;
+
+    }
+
+    void combHelper(int index, int sum, int target,vector<int> candidates,vector<int> &ele,vector<vector<int> > &list){
+        if(sum > target)
+            return;
+        if(sum == target){
+            list.push_back(ele);
+        }
+            
+        if(index > 0 && sum < target)
+            index--;
+
+        for(int i = index; i < candidates.size;++i){
+            ele.push_back(candidates[i]);
+            combHelper(index+1,sum+candidates[i],target,candidates,ele,list);
+            ele.pop_back();
+        }
+
+    }
+};
