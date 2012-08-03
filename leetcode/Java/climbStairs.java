ad4714d56c71168f550767e35cd8f239a6834399
wenqiang <wmiao1122@gmail.com>
2012-08-03 11:58:02 -0400

    add

--- leetcode/Java/climbStairs.java
+++ leetcode/Java/climbStairs.java
@@ -1,8 +1,18 @@
 public class climbStairs{
-    public int climbStairs(int n){
+    private int[] arr;
+    //naive recursion
+    public int climbStairs1(int n){
         if (n < 3)
             return n;
-        return climbStairs(n-1)+climbStairs(n-2);
+        return climbStairs1(n-1)+climbStairs1(n-2);
+    }
+
+    //DP Top down
+    public int climbStairs2(int n){
+        if( n < 3)
+            arr[n] = n;
+        arr[n] = climbStairs2[n-1]+climbStairs2[n-2];
+        return arr[n];
     }
 
 
