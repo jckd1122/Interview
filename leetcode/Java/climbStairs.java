c77ae6db058319b7c3939ea72e1abe367a19060a
jckd1122 <wmiao1122@gmail.com>
2012-08-03 13:38:47 -0300

    Update leetcode/Java/climbStairs.java

--- leetcode/Java/climbStairs.java
+++ leetcode/Java/climbStairs.java
@@ -11,8 +11,25 @@ public class climbStairs{
     public int climbStairs2(int n){
         if( n < 3)
             arr[n] = n;
-        arr[n] = climbStairs2(n-1)+climbStairs2(n-2);
-        return arr[n];
+        if(arr[n] == 0)
+            arr[n] = climbStairs2(n-1)+climbStairs2(n-2);
+        else
+            return arr[n];
+    }
+    
+    //DP bottom up
+    public int climbStairs(int n){
+        if(n < 3)
+            return n;
+        int temp1 = 1;
+        int temp2 = 2;
+        for(int i = 3; i <= n;++i){
+            temp2 = temp1+temp2;
+            temp1 = temp2-temp1;
+        }
+        return temp2;
+        
+        
     }
 
 
