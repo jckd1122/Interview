0a0de985b9f5f3097affaa44690673f359540b49
jckd1122 <wmiao1122@gmail.com>
2012-08-06 12:59:56 -0300

    Update leetcode/Java/minSumPath.java

--- leetcode/Java/minSumPath.java
+++ leetcode/Java/minSumPath.java
@@ -8,9 +8,9 @@ public class minSumPath{
         int n = grid[0].length;
         int[][] table = new int[m][n];
         table[0][0] = grid[0][0];
-        for(int j = 1; j < n-1;++j)
+        for(int j = 1; j < n;++j)
             table[0][j] = table[0][j-1]+grid[0][j];
-        for(int i = 1; i < m-1;++i)
+        for(int i = 1; i < m;++i)
             table[i][0] = table[i-1][0]+grid[i][0];
 
         for(int i = 1; i < m-1;++i){
