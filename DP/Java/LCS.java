7516e2c41513e0dea04dd25834fcf7fb16dfe374
wenqiang <wmiao1122@gmail.com>
2012-08-07 11:52:41 -0400

    update

--- DP/Java/LCS.java
+++ DP/Java/LCS.java
@@ -1,7 +1,8 @@
-import java.lang.math;
+import java.lang.String;
+import java.lang.Math;
 
 public class LCS{
-    public int lcs(String s1,String s2){
+    public static int lcs(String s1,String s2){
         int m = s1.length();
         int n = s2.length();
         int[][] table = new int[m][n];
@@ -11,23 +12,23 @@ public class LCS{
 
         for(int i = 0; i < m;++i){
             if(s1.charAt(i) == s2.charAt(0)){
-                for(int j = 0; j < m;++j{
-                    table[0][j] = 1
+                for(int j = 0; j < m;++j){
+                    table[0][j] = 1;
                 }
             }
         }
 
         for(int i = 0; i < n;++i){
             if(s2.charAt(i) == s1.charAt(0)){
-                for(int j = 0; j < n;++j{
-                    table[j][0] = 1
+                for(int j = 0; j < n;++j){
+                    table[j][0] = 1;
                 }
             }
         }
 
         for(int i = 1;i < m;++i){
             for(int j = 1;j < n;++j){
-                if(s1.charAt[i] == s2.charAt[j]{
+                if(s1.charAt(i) == s2.charAt(j)){
                     table[i][j] = 1+table[i-1][j-1];
                 }
                 else{
@@ -37,25 +38,27 @@ public class LCS{
 
         }
 
-        printSequence(table);
+        printSequence(s1,s2,table);
         return table[m-1][n-1];
 
 
     }
     
-    public static void printSequence(int[][] table){
-        int m = table.length();
-        int n = table[0].length();
+    public static void printSequence(String s1,String s2,int[][] table){
+        int m = table.length;
+        int n = table[0].length;
         
         int lValue = 0;
         int uValue = 0;
-        while(m >= 0 && n >=0){
+        while(true){
+            if(m == 1 || n == 1)
+                break;
             lValue = table[m-1][n-2];
             uValue = table[m-2][n-1];
             if(table[m-1][n-1] >  Math.max(lValue,uValue)){
                 m--;
                 n--;
-                System.out.print(table[m-1][n-1]+" ");
+                System.out.print(s1.charAt(m-1)+" ");
             }
             else if(lValue > uValue)
                 n--;
@@ -67,8 +70,8 @@ public class LCS{
     }
 
     public static void main(String[] args){
-        String s1 = "abcdgh";
-        String s2 = "aedfhr";
+        String s1 = "abcdefghijklmnzyxwvutsrqpo";
+        String s2 = "opqrstuvwxyzabcdefghijklmn";
         System.out.println(lcs(s1,s2));
 
     }
