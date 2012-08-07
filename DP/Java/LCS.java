c5df55450ca2909eff0acdb4fb63d5fb21c2f931
wenqiang <wmiao1122@gmail.com>
2012-08-07 13:56:40 -0400

    update

--- DP/Java/LCS.java
+++ DP/Java/LCS.java
@@ -5,30 +5,22 @@ public class LCS{
     public static int lcs(String s1,String s2){
         int m = s1.length();
         int n = s2.length();
-        int[][] table = new int[m][n];
+        int[][] table = new int[m+1][n+1];
 
         if(m == 0 || n == 0)
             return 0;
 
-        for(int i = 0; i < m;++i){
-            if(s1.charAt(i) == s2.charAt(0)){
-                for(int j = 0; j < m;++j){
-                    table[0][j] = 1;
-                }
-            }
+        for(int i = 0; i < m+1;++i){
+            table[i][0] = 0;
         }
 
-        for(int i = 0; i < n;++i){
-            if(s2.charAt(i) == s1.charAt(0)){
-                for(int j = 0; j < n;++j){
-                    table[j][0] = 1;
-                }
-            }
+        for(int j = 0; j < n;++j){
+            table[0][j] = 0;
         }
 
-        for(int i = 1;i < m;++i){
-            for(int j = 1;j < n;++j){
-                if(s1.charAt(i) == s2.charAt(j)){
+        for(int i = 1;i < m+1;++i){
+            for(int j = 1;j < n+1;++j){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
                     table[i][j] = 1+table[i-1][j-1];
                 }
                 else{
@@ -39,26 +31,26 @@ public class LCS{
         }
 
         printSequence(s1,s2,table);
-        return table[m-1][n-1];
+        return table[m][n];
 
 
     }
     
     public static void printSequence(String s1,String s2,int[][] table){
-        int m = table.length;
-        int n = table[0].length;
+        int m = table.length-1;
+        int n = table[0].length-1;
         
         int lValue = 0;
         int uValue = 0;
         while(true){
-            if(m == 1 || n == 1)
+            if(m == 0 || n == 0)
                 break;
-            lValue = table[m-1][n-2];
-            uValue = table[m-2][n-1];
-            if(table[m-1][n-1] >  Math.max(lValue,uValue)){
+            lValue = table[m][n-1];
+            uValue = table[m-1][n];
+            if(table[m][n] >  Math.max(lValue,uValue)){
+                System.out.print(s1.charAt(m-1)+" ");
                 m--;
                 n--;
-                System.out.print(s1.charAt(m-1)+" ");
             }
             else if(lValue > uValue)
                 n--;
