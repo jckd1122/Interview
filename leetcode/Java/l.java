74b1bc5f390e084adfabbb6a1a5f8e88a2287cf6
wenqiang <wmiao1122@gmail.com>
2012-08-08 12:14:19 -0400

    update

---
+++ leetcode/Java/l.java
@@ -0,0 +1,44 @@
+import java.lang.Math;
+import java.util.Stack;
+
+public class l{
+    public static int longestValidParentheses(String s){
+        int n = s.length();
+        int maxSoFar = 0;
+        int prevEnd = 0;
+        int curEnd = 0;
+        Stack<Character> stack = new Stack<Character>();
+        if(n == 0) return 0;
+        for(int i = 0; i < n;++i){
+            if(s.charAt(i) == '{'){
+                prevEnd = curEnd;
+                curEnd = 0;
+                stack.clear();
+                stack.push('{');
+            }
+            else{
+                if(stack.empty())
+                    curEnd = curEnd+prevEnd;
+                else{
+                    stack.pop();
+                    curEnd++;
+                    curEnd = Math.max(curEnd,prevEnd);
+                }
+            }
+            maxSoFar = Math.max(Math.max(curEnd,prevEnd),maxSoFar);
+
+        }
+        return maxSoFar;
+
+    }
+
+    public static void main(String[] args){
+        String s = "()(())";
+        System.out.println(longestValidParentheses(s));
+
+    }
+
+
+
+
+}
