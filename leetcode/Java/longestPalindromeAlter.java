60f38173bcac269a03ca1709968d09eb1d5e71d0
wenqiang <wmiao1122@gmail.com>
2012-08-09 10:53:04 -0400

    u

--- leetcode/Java/longestPalindromeAlter.java
+++ leetcode/Java/longestPalindromeAlter.java
@@ -3,11 +3,12 @@ import java.lang.String;
 public class Solution{
     public String longestPalindrome(String s){
         int l = s.length();
-        int maxSoFar = 0;
+        int maxSoFarOdd = 0;
+        int maxSoFarEven = 0
         int curEnd = 0;
         int p = 0;  //start index for masSoFar
         int q = 0;
-        int m = 0;  //start index for curEnd
+        int m = 1;  //start index for curEnd
         int n = 0;
         for(int i = 0; i < l;++i){
             if(s.charAt(i) == s.charAt(m-1)){
@@ -19,14 +20,41 @@ public class Solution{
                 n = n+1;
             }
             curEnd = n-m+1;
-            if(curEnd > maxSoFar){
+            if(curEnd > maxSoFarOdd){
                 p = m;
                 q = n;
-                maxSoFar = q-p+1;
+                maxSoFarOdd = q-p+1;
             }
         }
 
-        return s.substring(p,q+1);
+        p1 = 0;
+        q1 = 0;
+        m1 = 0;
+        n1 = 0;
+        curEnd = 0;
+
+        
+        for(int i = 0; i < l;++i){
+            if(s.charAt(i) == s.charAt(m1)){
+                m1--;
+                n1++;
+            }
+            else{
+                m1 = n1+1;
+                n1 = n1+1;
+            }
+            curEnd = n1-m1+1;
+            if(curEnd > maxSoFarEven){
+                p1 = m1;
+                q1 = n1;
+                maxSoFarEven = q1-p1+1;
+            }
+
+        }
+        if(maxSoFarEven > maxSoFarOdd)
+            return s.substring(p1,q1+1);
+        else
+            return s.substring(p,q+1);
     }
 
     public boolean ifPalindrome(String s){
