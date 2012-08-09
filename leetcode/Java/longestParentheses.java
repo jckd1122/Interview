513e8ccffd895a3ea1b64d22fdd083164d20e1e2
jckd1122 <wmiao1122@gmail.com>
2012-08-09 00:36:02 -0300

    Update leetcode/Java/longestParentheses.java

--- leetcode/Java/longestParentheses.java
+++ leetcode/Java/longestParentheses.java
@@ -1,43 +1,33 @@
 import java.lang.Math;
 import java.util.Stack;
 
-public class l {
-     public static int longestValidParentheses(String s){
+public class Solution {
+   public static int longestValidParentheses(String s){
         int n = s.length();
         int maxSoFar = 0;
         int curEnd = 0;
-        int start = n+1;
+        int start = 0;
         Stack<Integer> stack = new Stack<Integer>();
 
-        if(n == 0) return 0;
         for(int i = 0; i < n;++i){
-            //prevEnd = (curEnd == 0)? 0:curEnd;
             if(s.charAt(i) == '(' ){
                 stack.push(i);
                 continue;
-            } 
-            
+            }
+            if(stack.empty())
+                start = n;
             if(!stack.empty()){
-                //start = Math.min(stack.peek(),start);
+                start = Math.min(start,stack.peek());
                 stack.pop();
                     
-                if(!stack.empty())
-                    curEnd = i-stack.peek();
-                else
+                if(stack.empty()){
                     curEnd = i-start+1;
-                
+                }
+                else
+                    curEnd = i-stack.peek();
             }    
-            
             maxSoFar = Math.max(curEnd,maxSoFar);
-        
         }
         return maxSoFar;
      }
-
-
-    public static void main(String[] agrs){
-        String s = ")(((((()())()()))()(()))(";
-        System.out.println(longestValidParentheses(s));
-
-    }
 }
