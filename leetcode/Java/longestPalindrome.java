b93746025c656fcbc550f10c76ea2bcddbeb8b72
jckd1122 <wmiao1122@gmail.com>
2012-08-09 21:40:16 -0300

    Update leetcode/Java/longestPalindrome.java

--- leetcode/Java/longestPalindrome.java
+++ leetcode/Java/longestPalindrome.java
@@ -3,47 +3,26 @@ import java.lang.String;
 public class Solution{
     public String longestPalindrome(String s){
         int n = s.length();
-        int max = 0;
         int start = 0;
         int end = 0;
         int p = 0;
         int q = 0;
-        int l = 0;
+
         for(int i = 0; i < n;++i){
             end = n-1;
             start = i;
             
             while(true){
-                if(start > end){
-                    end = end+l;
-                    start = i;
+                if(ifPalindrome(s.substring(i,end+1)))
                     break;
-                }
-                
-                if(s.charAt(start) == s.charAt(end)){
-                    //l = start-end+1;
-                    
-                    start++;
+                else
                     end--;
-                    l++;
-                
-                }
-                else{
-                    start = i;
-                    end = l+end-1;
-                    l = 0;
-                    
-                    //q = end;
-                    
-                }
-
             }
             if(end-start >= q-p){
                 p = start;
                 q = end;
             }
         }
-            
         return s.substring(p,q+1);
     }
 
@@ -55,7 +34,4 @@ public class Solution{
         }
         return true;
     }
-
-
-
 }
