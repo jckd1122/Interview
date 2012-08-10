99fcc5e978dffded4bb5aa443e6d09e973fd40b4
jckd1122 <wmiao1122@gmail.com>
2012-08-10 01:10:18 -0300

    Update leetcode/Java/longestPalindromeAlter.java

--- leetcode/Java/longestPalindromeAlter.java
+++ leetcode/Java/longestPalindromeAlter.java
@@ -1,64 +1,46 @@
-import java.lang.String;
-//need to update
-public class longestPalindromeAlter{
+public class Solution {
     public static String longestPalindrome(String s){
         int n = s.length();
         int l,u;
-        int p,q;
+        int p = 0;
+        int q = 0;
 
-        for(int i = 0; i < n;++i){
-        
+        for(int i = 0; i < n-1;++i){
             //if s[i] is in the middle
-            
-        
-            if(s.charAt(i) != s.charAt(i+1)){
+            if(s.charAt(i) == s.charAt(i+1)){
                 l = i;
-                u = i;
-                while(l >= 1 && u <= n-1){
+                u = i+1;
+                while(l > 0 && u < n-1){
                     l--;
                     u++;
-                    if(s.charAt(l) != s.charAt(u))
+                    if(s.charAt(l) != s.charAt(u)){
+                        l++;
+                        u--;
                         break;
-
+                    }
                 }
                 if(q-p < u-l){
                     q = u;
                     p = l;
                 }
             }
-
-
-
             //s[i] and s[i+1] is in the middle
             l = i;
-            u = i+1;
-            while(l >=1 && u <= n-1){
+            u = i;
+            while(l > 0 && u < n-1){
                 l--;
                 u++;
-                if(s.charAt(l) != s.charAt(u))
+                if(s.charAt(l) != s.charAt(u)){
+                    l++;
+                    u--;
                     break;
+                }
             }
             if(q-p < u-l){
                 q = u;
                 p = l;
             }
-
         }
         return s.substring(p,q+1);
     }
-
-    
-
-
-    public static void main(String[] args){
-        String s = "abcba";
-        System.out.println(longestPalindrome(s));
-
-
-    }
-
-    
-
-
-
 }
