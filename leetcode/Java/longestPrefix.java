94621560dc47443ea185166024f7d7aaea80eb22
jckd1122 <wmiao1122@gmail.com>
2012-08-10 12:15:03 -0300

    Update leetcode/Java/longestPrefix.java

--- leetcode/Java/longestPrefix.java
+++ leetcode/Java/longestPrefix.java
@@ -16,10 +16,12 @@ public class Solution {
        
         for(int i = 0; i < first.length();++i){
             for(int j = 1; j < strs.length;++j){
-                if(i < strs[j].length() && first.charAt(i) == strs[j].charAt(i))
-                    continue;    
-                else if(i >= strs[j].length())
+                if(i >= strs[j].length()){
+                    finish = false;
                     break;
+                }
+                else if(first.charAt(i) == strs[j].charAt(i))
+                    continue;    
                 else {
                     finish = false;
                     break;
