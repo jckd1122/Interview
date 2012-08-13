52aaaa963fd000a786657a962ab26fbfd0b33c16
wenqiang <wmiao1122@gmail.com>
2012-08-13 11:00:02 -0400

    add

---
+++ leetcode/C++/median.cpp
@@ -0,0 +1,38 @@
+
+class Solution{
+public:
+    double find(int[] A,int m,int[] B,int n){
+        int l = 0;
+        int u = m-1;
+        int p = 0;
+        int q = n-1;
+        int mid1,mid2,move;
+
+        while(u > l && q > p){
+            mid1 = (l+u)/2;
+            mid2 = (p+q)/2;
+
+            if(A[mid1] == B[mid2])
+                return A[mid1];
+            else if(A[mid1] > B[mid2]){
+                if(A[mid1-1] < B[mid2+1])
+                    return (A[mid-1]+B[mid2+1])/2; 
+
+                u = (mid1 < mid2)? mid1-1:mid2-1;
+                p = (mid1 < mid2)? mid1+1:mid2+1;
+
+            }
+            else{
+                if(A[mid+1] > B[mid2-1])
+                    retrun (A[mid+1]+B[mid2-1])/2;
+                u = (mid1 < mid2)? mid1+1:mid2+1;
+                p = (mid1 < mid2)? mid1-1:mid2-1;
+                
+            }
+        }
+
+    }
+
+
+
+};
