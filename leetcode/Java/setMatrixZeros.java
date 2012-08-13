401cd0403c2c22fd31804498da6fae05aa9b6dac
jckd1122 <wmiao1122@gmail.com>
2012-08-13 16:21:04 -0300

    Update leetcode/Java/setMatrixZeros.java

--- leetcode/Java/setMatrixZeros.java
+++ leetcode/Java/setMatrixZeros.java
@@ -3,14 +3,14 @@ public class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        for(int i = 0; i < m;++i){
-            for(int j = 0; j < n;++j){
+        for(int i = m-1; i >= 0;--i){
+            for(int j = n-1; j >= 0;--j){
                 if(matrix[i][j] != 0){
-                    for(int k = j+1; k < n;++k){
+                    for(int k = 0; k < j;++k){
                         if(matrix[i][k] == 0)
                             matrix[i][j] = 0;
                     }
-                    for(int k = i+1; k < m;++k){
+                    for(int k = 0; k < i;++k){
                         if(matrix[k][j] == 0)
                             matrix[i][j] = 0;
                     }
