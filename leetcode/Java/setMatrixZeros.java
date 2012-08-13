9ee42be1e69dddb3985041c069ebece0e3e04610
jckd1122 <wmiao1122@gmail.com>
2012-08-13 18:10:40 -0300

    Update leetcode/Java/setMatrixZeros.java

--- leetcode/Java/setMatrixZeros.java
+++ leetcode/Java/setMatrixZeros.java
@@ -2,29 +2,51 @@ public class Solution {
       public void setZeroes(int[][] matrix){
         int m = matrix.length;
         int n = matrix[0].length;
+        int tmp1 = 0;
+        int tmp2 = 0;
       
         for(int i = 0; i < m;++i){
             for(int j = 0; j < n;++j){
                 if(matrix[i][j] == 0){
-                      matrix[0][j]= 1;     //for col
-                      matrix[i][0] = 1;    //for row
+                    matrix[0][j]= 1;     //for col
+                    matrix[i][0] = 1;    //for row
+                    if(i == 0)
+                        tmp1 = 1;
+                    if(j == 0)
+                        tmp2 = 1;
+                   
                 }
             }
         }
         
-        for(int i = 0; i < m; ++i){
-            if(matrix[i][0]){
+         
+       
+        for(int i = 1; i < m; ++i){
+            if(matrix[i][0] == 1){
                 for(int j = 0; j < n;++j){
                     matrix[i][j] = 0;
                 }
             }
         }
         
-        for(int j = 0; j < n; ++j){
-            if(matrix[0][j]){
+        for(int j = 1; j < n; ++j){
+            if(matrix[0][j] == 1){
                 for(int i = 0; i < m;++i){
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        
+         
+        if(tmp1 == 1){
+            for(int j = 0; j < n;++j){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(tmp2 == 1){
+            for(int i = 0; i < m;++i){
+                matrix[i][0] = 0;
             }
         }
     }
