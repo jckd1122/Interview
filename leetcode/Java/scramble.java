1319715e643d1eb87ed0bd12d22c1431ce360d01
wenqiang <wmiao1122@gmail.com>
2012-08-14 10:42:02 -0400

    u

--- leetcode/Java/scramble.java
+++ leetcode/Java/scramble.java
@@ -2,14 +2,31 @@ public class Solution {
     public boolean isScramble(String s1, String s2) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        boolean[] isUsed = new boolean[];
         int n = s1.length();
         if(n == 1)
             return s1.charAt(0) == s2.charAt(0);
         for(int i = 0; i < n;++i){
-            if(s1.charAt(0) == s2.charAt(i)){
-                return isScramble(s1.substring(1,n),s2.substring(i+1,n)+s2.substring(0,i));
+            int j = i;
+            for(; j < n;++j){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    if(isUsed[j] == false)
+                        s2 = swap(s2,i,j);
+                    else
+                        return false;
+                    isUsed[j] = true;
+                    break;
+                }
             }
         }
-        return false;
+        return true;
+    }
+
+    String swap(String s, int i , int j){
+        char[] arr = s.toCharArray();
+        char tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+        return new String(arr);
     }
 }
