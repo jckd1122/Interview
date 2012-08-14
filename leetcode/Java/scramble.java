8bc6259b728a28e894c8af1ef823cf14ee2dc7ae
jckd1122 <wmiao1122@gmail.com>
2012-08-14 12:46:53 -0300

    Update leetcode/Java/scramble.java

--- leetcode/Java/scramble.java
+++ leetcode/Java/scramble.java
@@ -7,9 +7,9 @@ public class Solution {
         if(n == 1)
             return s1.charAt(0) == s2.charAt(0);
         for(int i = 0; i < n;++i){
-            if(isScramble(s1.substring(0,i+1),s2.substring(0,i+1)))
+            if(isScramble(s1.substring(0,i+1),s2.substring(0,i+1)) && isScramble(s1.substring(i+1,n),s2.substring(i+1,n)))
                 return true;
-            if(isScramble(s1.substring(0,i+1),s2.substring(n-i-1,n)))
+            if(isScramble(s1.substring(0,i+1),s2.substring(n-i-1,n)) $$ isScramble(s1.substring(i+1,n),s2.substring(0,i)))
                 return true;
         
         }
