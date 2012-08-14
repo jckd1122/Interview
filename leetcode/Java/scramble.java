c7878146d75a5180db29a7e0eec2600304e9494a
jckd1122 <wmiao1122@gmail.com>
2012-08-14 14:31:56 -0300

    Update leetcode/Java/scramble.java

--- leetcode/Java/scramble.java
+++ leetcode/Java/scramble.java
@@ -1,4 +1,3 @@
-import java.lang.String;
 public class Solution {
     public boolean isScramble(String s1, String s2) {
         int n = s1.length();
@@ -11,7 +10,7 @@ public class Solution {
         return isScrambleHelper(s1,s2,table1,table2,table3,table4);
     }
     
-    public boolean isScrambleiHelper(String s1, String s2,int[] table1,int[] table2,int[] table3,int[] table4) {
+    public boolean isScrambleHelper(String s1, String s2,int[] table1,int[] table2,int[] table3,int[] table4) {
         // Start typing your Java solution below
         // DO NOT write main() function
         int n = s1.length();
@@ -19,19 +18,20 @@ public class Solution {
             return s1.charAt(0) == s2.charAt(0);
         for(int i = 0; i < n-1;++i){
             if(table1[i] == 0)
-                table1[i] = isScramble(s1.substring(0,i+1),s2.substring(0,i+1));
+                table1[i] = (isScramble(s1.substring(0,i+1),s2.substring(0,i+1)) == true)? 1:-1;
             if(table2[i] == 0)
-                table2[i] = isScramble(s1.substring(i+1,n),s2.substring(i+1,n));
+                table2[i] = (isScramble(s1.substring(i+1,n),s2.substring(i+1,n)) == true)? 1:-1;
             if(table3[i] == 0)
-                table3[i] = isScramble(s1.substring(0,i+1),s2.substring(n-i-1,n));
+                table3[i] = (isScramble(s1.substring(0,i+1),s2.substring(n-i-1,n)) == true)? 1:-1;
             if(table4[i] == 0)
-                table4[i] = isScramble(s1.substring(i+1,n),s2.substring(0,n-i-1));
+                table4[i] = (isScramble(s1.substring(i+1,n),s2.substring(0,n-i-1)) == true)? 1:-1;
             
-            if(table1[i] && table2[i])
+            if(table1[i] == 1 && table2[i] == 1)
                 return true;
-            if(table3[i] && table4[i])
+            if(table3[i] == 1 && table4[i] == 1)
                 return true;
         
         }
         return false;
+    }
 }
