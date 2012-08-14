0d633b5dd1b5842cfe24259669f41c35dd2a7bb0
jckd1122 <wmiao1122@gmail.com>
2012-08-14 18:48:19 -0300

    Update leetcode/Java/pow.java

--- leetcode/Java/pow.java
+++ leetcode/Java/pow.java
@@ -3,17 +3,25 @@ public class Solution{
         double result = 1;
         if(n == 0)
             return 1.0;
-        while(n > 0){
-            result *= x;
-            n--;
-        }
         
-        while(n < 0){
-            result *= 1/x;
-            n++;
+        if(n%2 == 0){
+            n /= 2;
+            result = pow(x,n)*pow(x,n);
+        }
+        else{
+            if( n > 0){
+                n = (n-1)/2;
+                 result = pow(x,n)*pow(x,n)*x;
+            }
+            else{
+                n = (n+1)/2;
+                result = pow(x,n)*pow(x,n)*1/x;
+            }
         }
         return result;
     }
+    
+    
 
 
 }
