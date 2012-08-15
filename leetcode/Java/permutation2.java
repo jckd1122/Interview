72463242ad0e6ca3c2ec942d6334b4e011703a00
jckd1122 <wmiao1122@gmail.com>
2012-08-15 12:43:05 -0300

    Update leetcode/Java/permutation2.java

--- leetcode/Java/permutation2.java
+++ leetcode/Java/permutation2.java
@@ -1,1 +1,40 @@
-pub
+import java.util.*;
+import java.util.Arrays;
+public class Solution{
+    public ArrayList<ArrayList<Integer> > permute(int[] num){
+        ArrayList<ArrayList<Integer> > list = new ArrayList<ArrayList<Integer> >();
+        permuteHelper(list,num,0);
+        return list;
+        
+    }
+
+    public void  permuteHelper(ArrayList<ArrayList<Integer> > list,int[] num,int start){
+        int n = num.length;
+        if(start > n-1){    
+            ArrayList<Integer> numList = new ArrayList<Integer>();
+            for(int i = 0; i < n;++i)
+                numList.add(num[i]);
+            
+            list.add(numList);
+            return;
+        }
+        for(int j = start; j < n;++j){
+            swap(num,start,j);
+            permuteHelper(list,num,start+1);
+            swap(num,start,j);
+         
+          
+        }
+
+
+    }
+
+    void swap(int[] arr,int i,int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+
+
+}
