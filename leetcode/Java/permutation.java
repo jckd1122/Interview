1584a202a32116031a65491afcf7f07ffa968b82
jckd1122 <wmiao1122@gmail.com>
2012-08-15 01:39:54 -0300

    Update leetcode/Java/permutation.java

--- leetcode/Java/permutation.java
+++ leetcode/Java/permutation.java
@@ -1,22 +1,28 @@
 import java.util.*;
 public class Solution{
-    public ArrayList<ArrayList<Integer> > permuter(int[] num){
+    public ArrayList<ArrayList<Integer> > permute(int[] num){
         ArrayList<ArrayList<Integer> > list = new ArrayList<ArrayList<Integer> >();
-        permuterHelper(list,num,0,num.length-1);
+        permuteHelper(list,num,0,num.length-1);
         return list;
+        
     }
 
     public void  permuteHelper(ArrayList<ArrayList<Integer> > list,int[] num,int start,int end){
         int n = num.length;
-        for(int j = start; j < end;++j){
-            swap(num,i,j);
+        if(start > end)
+            return;
+        for(int j = start; j <= end;++j){
+            swap(num,start,j);
             ArrayList<Integer> ele = new ArrayList<Integer>();
-            ele.add(num[i]);
-            ArrayList<ArrayList<Integer> > sublist = permuter(num,start+1,end);
+            permuteHelper(list,num,start+1,end);
+            ArrayList<ArrayList<Integer> > list1 = new ArrayList<ArrayList<Integer> >(list);
+            ArrayList<ArrayList<Integer> > sublist = list1;
+            list.clear();
             for(ArrayList<Integer> m : sublist){
+                
                 ele.clear();
-                ele.add(num[i]);
-                ele.add(m);
+                ele.add(num[start]);
+                ele.addAll(m);
                 list.add(ele);
             }
         }
