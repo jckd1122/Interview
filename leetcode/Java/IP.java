9264a3b4f194e3ecc843cc023854a76d78ac23b2
wenqiang <wmiao1122@gmail.com>
2012-08-15 17:37:41 -0400

    add

---
+++ leetcode/Java/IP.java
@@ -0,0 +1,29 @@
+
+public class Solution{
+    public ArrayList<String> restoreIpAddresses(String s){
+        ArrayList<String> list = new ArrayList<String>();
+        restoreHelper(list,s,"");
+
+    }
+
+    void restoreIpHelper(ArrayList<String> list,String restStr,String cur){
+        int n = restStr.length();
+        if(n == 0)
+            list.add(cur);
+
+       for(int i = 0; i < n;++i){
+           if((i == 3 && Integer.parseInt(restStr.substring(0,3)) > 255) || (i > 3))
+               break;
+           string added = restStr.substring(0,i);
+           restoreIpHelper(list,restStr.substring(i,n,cur+added);)
+
+       }
+
+
+    }
+
+
+
+
+
+}
