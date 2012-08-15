5c82c3c0671027f42cb94d770f9c1397174a744e
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-08-15 00:16:35 -0400

    u

--- leetcode/Java/permutation.java
+++ leetcode/Java/permutation.java
@@ -1,33 +1,33 @@
 import java.util.*;
 public class Solution{
-    public ArrayList<ArrayList<Integer> > permute(int[] num){
-        int n = num.length;
-        ArrayList<ArrayList<Integer> > baseList = new ArrayList<ArrayList<Integer> >();
-        ArrayList<Integer> baseEle = new ArrayList<Integer>();
-        if(n == 1){
-            baseEle.add(num[0]);
-            baseList.add(baseEle);
-            return baseList;
-        }
-
-            
-        int[] restNum = new int[n-1];
-        for(int i = 0; i < n-1;++i)
-            restNum[i] = num[i];
-
-        ArrayList<ArrayList<Integer> > list = permute(restNum);
-        for(int i = 0; i < list.size();++i){
-            ArrayList<Integer> ele = list.get(i);
-            for(int j = 0;j < ele.size();++j){
-                ArrayList<Integer> newEle = ele;
-                newEle.add(num[n-1],j);
-                list.add(newEle);
-            }
-            ele.add(num[n-1]);
-
-        }
+    public ArrayList<ArrayList<Integer> > permuter(int[] num){
+        ArrayList<ArrayList<Integer> > list = new ArrayList<ArrayList<Integer> >();
+        permuterHelper(list,num,0,num.length-1);
         return list;
+    }
 
+    public void  permuteHelper(ArrayList<ArrayList<Integer> > list,int[] num,int start,int end){
+        int n = num.length;
+        for(int j = start; j < end;++j){
+            swap(num,i,j);
+            ArrayList<Integer> ele = new ArrayList<Integer>();
+            ele.add(num[i]);
+            ArrayList<ArrayList<Integer> > sublist = permuter(num,start+1,end);
+            for(ArrayList<Integer> m : sublist){
+                ele.clear();
+                ele.add(num[i]);
+                ele.add(m);
+                list.add(ele);
+            }
+        }
+
+
+    }
+
+    void swap(int[] arr,int i,int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
 
