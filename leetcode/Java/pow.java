ee3f8db52801e6112a0327029b1fadaa509792c1
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-08-14 20:15:51 -0400

    add

--- leetcode/Java/pow.java
+++ leetcode/Java/pow.java
@@ -1,31 +1,14 @@
 public class Solution{
     public double pow(double x, int n){
-        double result = 1;
-        double tmp;
+        double tmp,a;
+        if(n < 0)
+            return 1/pow(x,-n);
         if(n == 0)
             return 1.0;
         
-        if(n%2 == 0){
-            n /= 2;
-            tmp = pow(x,n);
-            result = tmp*tmp;
-        }
-        else{
-            if( n > 0){
-                n = (n-1)/2;
-                tmp = pow(x,n);
-                 result = tmp*tmp*x;
-            }
-            else{
-                n = (n+1)/2;
-                tmp = pow(x,n);
-                result = tmp*tmp*1/x;
-            }
-        }
-        return result;
+        a = (n%2 == 1)? x:1;
+        n /= 2;
+        tmp = pow(x,n);
+        return tmp*tmp*a;
     }
-    
-    
-
-
 }
