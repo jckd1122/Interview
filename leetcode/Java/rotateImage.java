3a65ffae4bf2763d26181c0add3fc2dbb84477f1
jckd1122 <wmiao1122@gmail.com>
2012-08-16 01:34:33 -0300

    Update leetcode/Java/rotateImage.java

--- leetcode/Java/rotateImage.java
+++ leetcode/Java/rotateImage.java
@@ -1,18 +1,17 @@
-
 public class Solution{
     public void rotate(int[][] matrix){
         int m = matrix.length;
         int n = matrix[0].length;
 
         for(int i = 0; i < m;++i){
-            for(int k = n-1; k > 0;--k){
+            for(int k = n-1; k > i;--k){
                 matrix[k][n-i-1] = matrix[i][k];     //copy value to its future location
 
             }
-            for(int j = 0; j < n;++j){
+            for(int j = 0; j < n-i;++j){
                 matrix[i][j] = matrix[n-j-1][i];
             }
-            n--;
+            
 
         }
 
