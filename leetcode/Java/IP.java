183783d50f3042c395a0e8831c017c93023af416
jckd1122 <wmiao1122@gmail.com>
2012-08-15 21:20:14 -0300

    Update leetcode/Java/IP.java

--- leetcode/Java/IP.java
+++ leetcode/Java/IP.java
@@ -1,31 +1,51 @@
 import java.util.*;
 public class Solution {
-    public ArrayList<String> restoreIpAddresses(String s) {
+    public static ArrayList<String> restoreIpAddresses(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
         
         ArrayList<String> list = new ArrayList<String>();
-        restoreHelper(list,s,"");
+        int l = s.length()+4;
+        restoreHelper(list,s,"",l);
         return list;
 
 
     }
 
-    void restoreHelper(ArrayList<String> list,String restStr,String cur){
+    static void restoreHelper(ArrayList<String> list,String restStr,String cur,int l){
         int n = restStr.length();
-        if(n == 0)
-            list.add(cur);
+        if(restStr == ""){
+            if(cur.length() == l){ 
+                list.add(cur.substring(0,cur.length()-1));
+                
+            }
+            return;
+            
+        }
 
-       for(int i = 0; i < n;++i){
+       for(int i = 1; i <= n;++i){
            if((i == 3 && Integer.parseInt(restStr.substring(0,3)) > 255) || (i > 3))
                break;
-           String added = restStr.substring(0,i);
-           if(current.length() 
-           restoreHelper(list,restStr.substring(i,n),cur+added);
+           String added = restStr.substring(0,i)+".";
+           
+           
+           restoreHelper(list,restStr.substring(i,n),cur+added,l);
 
        }
 
 
     }
 
+       
+
+
+    
+    
 }
+    
+    
+    
+    
+    
+    
+ 
