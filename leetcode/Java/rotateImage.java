de876eb56ac24c23a1ec425766bc6d49b1afc7f0
wenqiang <wmiao1122@gmail.com>
2012-08-16 14:26:51 -0400

    add

--- leetcode/Java/rotateImage.java
+++ leetcode/Java/rotateImage.java
@@ -3,21 +3,32 @@ public class Solution{
         int m = matrix.length;
         int n = matrix[0].length;
 
-        for(int i = 0; i < m;++i){
-            for(int k = n-1; k > i;--k){
-                matrix[k][n-i-1] = matrix[i][k];     //copy value to its future location
+        for(int i = 0; i < n/2;++i){
+            for(int j = i; j < n-i-1;j++){
+                swap(matrix,i,j);     
 
             }
-            for(int j = 0; j < n-i;++j){
-                matrix[i][j] = matrix[n-j-1][i];
-            }
-            
-
+        
         }
-
-
-
     }
+
+        //four way swap
+    void swap(int[][] arr, int i, int j){
+        int first = arr[i][j];
+        int second = arr[j][n-i-1];
+        int third = arr[n-i-1][n-j-1];
+        int fourth = arr[n-j-1][i];
+
+        int tmp1 = second;
+        int tmp2 = fourth;
+        second = first;
+        fourth = third;
+        first = tmp2;
+        third = tmp1;
+    }
+
+
+    
 
 
 
