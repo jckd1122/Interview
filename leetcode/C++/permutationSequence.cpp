ad07908fe809ce47a17e3f742ea6349c9b2ac54b
jckd1122 <wmiao1122@gmail.com>
2012-08-18 15:29:09 -0300

    Update leetcode/C++/permutationSequence.cpp

--- leetcode/C++/permutationSequence.cpp
+++ leetcode/C++/permutationSequence.cpp
@@ -4,8 +4,8 @@ class Solution {
 public:
     string getPermutation(int n, int k) {
         string num;
+        stringstream s;
         for(int i = 1; i <= n;++i){
-            stringstream s;
             s << i;
         }
         num = s.str();
