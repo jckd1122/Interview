ec5a337de3c1a6196a011c54853bb409438813b0
jckd1122 <wmiao1122@gmail.com>
2012-08-21 14:24:32 -0300

    Update leetcode/Java/reverseLinkedList.java

--- leetcode/Java/reverseLinkedList.java
+++ leetcode/Java/reverseLinkedList.java
@@ -12,32 +12,46 @@
 public class Solution{
     public ListNode reverseBetween(ListNode head, int m, int n){
         int length = 1;
-        ListNode l0 = head;
-        ListNode l1 = head;
-        ListNode l2 = head;
+        ListNode l0;
+        ListNode p1 = head;
+        ListNode p2,l1,l2,after1,after2;
+        p2 = head;
         while(l0.next != null){
             l0 = l0.next;
             length++;
         }
-        //if(length  m || length > n)
-        //    return head;
-        while(m-- > 1){
-            l1 = l1.next;
+        
+        if(length <=2){
+            
         }
-        while(n-- > 1){
-            l2 = l2.next;
+            return head;
+        while(m-- > 2){
+            p1 = p1.next;
+        }
+        while(n-- > 2){
+            p2 = p2.next;
         }
         
-        for(int)
-        int tmp = l1.val;
-        l1.val = l2.val;
-        l2.val = tmp;
+        l1 = p1.next;
+        after1 = p1.next.next;
+        l2 = p2.next;
+        after2 = p2.next.next;
+        while(l1 != l2){
+            l2.next = l1;
+            p2.next = after2;
+            p1.next = l2;
+            
+            p1 = p1.next;
+            l1 = l1.next;
+            after1= after1.next;
+            p2 = p2.next;
+            l2 = l2.next;
+            after2 = after2.next;
+        }
         return head;
 
     }
     
     
-
-
-
 }
+
