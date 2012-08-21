3680823bf8fc3f3559208fb836f617dc57a8ed49
jckd1122 <wmiao1122@gmail.com>
2012-08-21 16:27:18 -0300

    Update leetcode/Java/countAndSay.java

--- leetcode/Java/countAndSay.java
+++ leetcode/Java/countAndSay.java
@@ -4,7 +4,7 @@ public class Solution{
         int[] buffer = new int[10];
         String prev = "1";
         String newPrev = "";
-        for(int i = 1; i <= n;++i){
+        for(int i = 2; i <= n;++i){
             int l = prev.length();
             int count = 1;
             int j = 1;
@@ -15,9 +15,11 @@ public class Solution{
                 count = 1;
                 
             }
+            if(l == 1)
+                buffer[prev.charAt(0)] = 1;
             //generare newPrev
             j = 0;
-            while(j < l-1){
+            while(j < l){
                 newPrev += (char)(buffer[prev.charAt(j)]+48); 
                 newPrev += prev.charAt(j);
                 j += buffer[prev.charAt(j)];
