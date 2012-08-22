afed4dc3c23ab0b2c452e6daea2e2088d1a5421d
jckd1122 <wmiao1122@gmail.com>
2012-08-22 17:35:49 -0300

    Update leetcode/Java/mergeSortedArray.java

--- leetcode/Java/mergeSortedArray.java
+++ leetcode/Java/mergeSortedArray.java
@@ -2,8 +2,8 @@ public class Solution{
     public void merge(int[] A, int m, int[] B, int n){
         //copy array A's content to get out of merge place
         for(int i = 0; i < m;++i)
-            A[i+n+m] = A[i];
-        int h1 = m+n;
+            A[i+m] = A[i];
+        int h1 = m;
         int h2 = 0;
         int k = 0;
         while(k++ < m+n){
