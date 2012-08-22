3dcc0fd4f84f5a495e8966276eec7aa0126672c7
jckd1122 <wmiao1122@gmail.com>
2012-08-22 02:45:26 -0300

    Update leetcode/Java/mergeTwoLists.java

--- leetcode/Java/mergeTwoLists.java
+++ leetcode/Java/mergeTwoLists.java
@@ -7,27 +7,33 @@ public class Solution{
         ListNode h2 = l2;
         ListNode p1,p2;
 
-        while(h1 != null && h2 != null){
+        while(h1.next != null || h2.next != null){
             p1 = h1;
             p2 = h2;
-            if(h1.val < h2.val && h1.next.val > h2.val){
+            if(h2.val >= h1.val){
                 h1 = h1.next;
-                p1.next = h2;
+                
+                if(h2.val <= h1.val){
+                    p1.next = h2;
+                }
             }
-            if(h1.val > h2.val && h2.next.val > h1.val){
+            else{
                 h2 = h2.next;
-                p2.next = h1;
+                if(h1.val <= h2.val){
+                    p2.next = h1;
+                }
             }
-            if(h2.val > h1.next.val){
-                h1 = h1.next;
-            }
-            if(h1.val > h2.next.val){
-                h2 = h2.next;
-            }
-
-
         }
-        return (h1 == null)? h2:h1;
+        
+        if(h1.val <= h2.val){
+            h1.next = h2;
+            h2 = null;
+        }
+        else{
+            h2.next = h1;
+            h1 = null;
+        }
+        return (h1 == null)? l2:l1;
 
 
     }
