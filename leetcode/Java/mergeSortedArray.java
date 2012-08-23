61584572d8b6cdab2314f3dd4b5055a6e462a163
jckd1122 <wmiao1122@gmail.com>
2012-08-23 00:24:00 -0300

    Update leetcode/Java/mergeSortedArray.java

--- leetcode/Java/mergeSortedArray.java
+++ leetcode/Java/mergeSortedArray.java
@@ -1,28 +1,21 @@
-public class mergeSortedArray{
-    public static void merge(int[] A, int m, int[] B, int n){
-        //copy array A's content to get out of merge place
+public class Solution{
+    public void merge(int[] A, int m, int[] B, int n){
         int h1 = n;
         int h2 = 0;
         int k = 0;
     
-        if(m == 0 ){
-            for(int i = 0; i < n;++i)
-                A[i] = B[i];
-            return;
-        }
-        
-        for(int i = 0; i < m;++i)
-            A[n+i] = A[i];
+        //copy array A's content to get out of merge place
+        int h1 = n;
+        for(int i = m+n-1; i >= n;--i)
+            A[i] = A[i-n];
             
         while(true){
-            if(h1 >= m+n){
+            if(h1 == m+n){
                 for(int i = h2; i < n;++i)
                     A[k++] = B[i];
                 break;
             }
             if(h2 >= n){
-                for(int i = h1; i < m+n;++i)
-                    A[k++] = A[i];
                 break;
             }
 
@@ -32,20 +25,7 @@ public class mergeSortedArray{
             else{
                A[k++] = B[h2++];
             }
-                
         }
-
-
     }
-
-    public static void main(String[] args){
-        int[] A = {1,2,3,0,0,0};
-        int[] B = {4,5,6};
-        merge(A,3,B,3);
-
-    }
-
-   
-
 
 }
