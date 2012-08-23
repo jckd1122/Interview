3888d7a5c16dab0fa8d0a3fa115154f6eb7c4c42
wenqiang <wmiao1122@gmail.com>
2012-08-23 16:26:20 -0400

    add

--- leetcode/Java/reverseLinkedList.java
+++ leetcode/Java/reverseLinkedList.java
@@ -11,42 +11,38 @@
  */
 public class Solution{
     public ListNode reverseBetween(ListNode head, int m, int n){
-        int length = 1;
-        ListNode l0;
-        ListNode p1 = head;
-        ListNode p2,l1,l2,after1,after2;
-        p2 = head;
-        while(l0.next != null){
-            l0 = l0.next;
-            length++;
-        }
+        if(m == n)
+            return head;
+
+        ListNode prev1 = head;
+        ListNode prev1Tmp,cur1,post1;
+        ListNode cur2 = head;
+        ListNode post2;
         
-        if(length < 2){
-            return head;
+        if(m == 1){
+            prev1 = new ListNode(Integer.MAX_VALUE);
+            prev1.next = head;
         }
-            return head;
         while(m-- > 2){
-            p1 = p1.next;
+            prev1 = prev1.next;
         }
-        while(n-- > 2){
-            p2 = p2.next;
+        while(n-- > 1){
+            cur2 = cur2.next;
         }
-        
-        l1 = p1.next;
-        after1 = p1.next.next;
-        l2 = p2.next;
-        after2 = p2.next.next;
-        while(l1 != l2){
-            l2.next = l1;
-            p2.next = after2;
-            p1.next = l2;
+        cur1 = prev1.next;
+        post1 = prev1.next.next;
+        post2 = cur2.next;
+
+        while(cur1 != cur2){
+            prev1Tmp = prev1;
+            prev1.next = post1;
+            cur2.next = cur1;
+            cur1.next = post2;
             
-            p1 = p1.next;
-            l1 = l1.next;
-            after1= after1.next;
-            p2 = p2.next;
-            l2 = l2.next;
-            after2 = after2.next;
+            prev1 = prev1Tmp;
+            cur1 = prev1.next;
+            post1 = prev1.next.next;
+            cur2 = cur2.next;
         }
         return head;
 
