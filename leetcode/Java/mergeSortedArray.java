373929f0f28a2106aae4117ecae41e087e183591
jckd1122 <wmiao1122@gmail.com>
2012-08-22 23:50:11 -0300

    Update leetcode/Java/mergeSortedArray.java

--- leetcode/Java/mergeSortedArray.java
+++ leetcode/Java/mergeSortedArray.java
@@ -1,52 +1,33 @@
 public class Solution{
     public void merge(int[] A, int m, int[] B, int n){
         //copy array A's content to get out of merge place
-        int h1 = 0;
+        int h1 = n;
         int h2 = 0;
         int k = 0;
-        boolean isSwapped = false;
+    
         if(m == 0 ){
             for(int i = 0; i < n;++i)
                 A[i] = B[i];
             return;
         }
-        for(int i = m; i < m+n;++i)
-            A[i] = 1000;
-        while(h1 < m+n && h2 < n){
-            if(isSwapped){
-                if(B[0] <= B[h2]){
-                    swap(A,B,h1,0);
-                    h1++;
-                }
-                else{
-                    A[h1] = B[h2];
-                    h2++;
-                }
-                continue;
-
-            }
-
-            if(A[h1] <= B[h2]){
-                h1++;
-            }
-            else{
-                swap(A,B,h1,0);
-                isSwapped = true;
-                h1++;
-                h2++;
-                
-            }
+        
+        for(int i = 0; i < m;++i)
+            A[n+i] = A[i];
+            
+        while(k < m+n){
+           if(A[h1] <= B[h2]){
+               A[k++] = A[h1++];
+           }
+           else{
+               A[k++] = B[h2++];
+           }
                 
         }
 
 
     }
 
-    void swap(int[] arr1,int[] arr2, int i, int j){
-        int tmp = arr1[i];
-        arr1[i] = arr2[j];
-        arr2[j]= tmp;
-    }
+   
 
 
 }
