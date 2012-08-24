ea569ffaa63ee19f89810eb10d383f0635aa6279
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-08-24 01:00:17 -0400

    add

--- leetcode/Java/reverseLinkedList.java
+++ leetcode/Java/reverseLinkedList.java
@@ -46,6 +46,36 @@ public class Solution{
         return head;
 
     }
+
+    public ListNode reverseBetween(ListNode head, int m, int n){
+        if(head == null || m == n)
+            return head;
+
+        //here prev corrsponds to the first element to be reversed
+        ListNode prev = head;// includes m == 1's case 
+        ListNode cur;
+        ListNode end;
+        int count = m+1;
+        while(m > 1){
+            prev = prev.next;
+            m--;
+        }
+        cur = prev.next;
+        end = prev;
+
+        while(count <= n){
+            ListNode post = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = post;
+            count++;
+        }
+        end.next = cur;
+        return head;
+
+
+
+    }
     
     
 }
