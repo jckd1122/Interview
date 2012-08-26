6d2fa53beb75b5070411f2b24c586f778c45ce70
jckd1122 <wmiao1122@gmail.com>
2012-08-26 17:10:04 -0300

    Update leetcode/Java/partition_List.java

--- leetcode/Java/partition_List.java
+++ leetcode/Java/partition_List.java
@@ -1,1 +1,43 @@
-public
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(head == null)
+            return head;
+            
+        ListNode h = head;
+        ListNode prev,next,last2;
+        ListNode last1 = new ListNode(-1);
+        prev = last1;
+        prev.next = head;
+        
+        
+        //ListNode next = head;
+        while(h != null){
+
+            if(h.val <= x){
+                last2 = last1.next;
+                last1.next = h;
+                last1 = last1.next;
+                last1.next = last2;
+                prev.next = next;
+            }
+            
+            prev = h;
+            next = h.next;
+            h = h.next;
+        }
+        return head;
+    }
+}
