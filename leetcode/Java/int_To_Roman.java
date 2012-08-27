0807a1f084706dd649436a6525f590ad31bc29ca
jckd1122 <wmiao1122@gmail.com>
2012-08-27 12:54:23 -0300

    Update leetcode/Java/int_To_Roman.java

--- leetcode/Java/int_To_Roman.java
+++ leetcode/Java/int_To_Roman.java
@@ -3,18 +3,14 @@ public class Solution{
         String[] letter = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int[] number ={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String ret = "";
-        for(int i = 0;i < 13;++i)
-            int l = num/number[i];
+        int l;
+        for(int i = 0;i < 13;++i){
+            l = num/number[i];
             for(int j = 1;j <= l;++j)
                 ret += letter[i];
             num -= l*number[i];
 
         }
         return ret;
-
-
     }
-
-
-
 }
