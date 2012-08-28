5c5326ddaea4b43598c3a1638fd80150876e4deb
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-08-28 00:42:01 -0400

    add

--- leetcode/Java/subsetI.java
+++ leetcode/Java/subsetI.java
@@ -1,33 +1,51 @@
-public class Solution{
-    public ArrayList<ArrayList<Integer> > subsets(int[] S){
-        int l = s.length;
+import java.util.*;
+
+public class subsetI{
+    public static ArrayList<ArrayList<Integer> > subsets(int[] S){
+        int l = S.length;
         ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
         if(l == 0)
             return list;
 
         for(int i = 1; i <=l;++i){
             ArrayList<Integer> buffer = new ArrayList<Integer>();
-            for(int j = 0; j <= l-i;++j){
+            for(int j = 0; j < l;++j){
                 helper(list,buffer,j,i,S);
             }
 
         }
-
+        return list;
 
     }
 
-    void helper(ArrayList<ArrayList<Integer>> list,ArrayList<Integer> buffer,int pos,int length,int[] S){
+    static void helper(ArrayList<ArrayList<Integer>> list,ArrayList<Integer> buffer,int pos,int length,int[] S){
         if(buffer.size() == length){
             list.add(buffer);
-            buffer.clear();
             return;
         }
 
-        buffer.add(S[i]);
-        for(int i = pos+1; i <= l-(length-buffer.size());++i){
-            helper(list,buffer,i,length,S);
+        
+        for(int i = pos; i < S.length;++i){
+            buffer.add(S[i]);
+            helper(list,buffer,i+1,length,S);
+            buffer.remove(buffer.size()-1);
 
         }
+
+    }
+
+    public static void main(String[] args){
+        int[] S = {1,2,3};
+        //ArrayList<ArrayList<Integer>> list = subsets(S);
+        System.out.println(subsets(S).size());
+        for(int i = 0; i < subsets(S).size();++i){
+            for(int j = 0; j < subsets(S).get(i).size();++j){
+                System.out.print(subsets(S).get(i).get(j));
+            }
+            System.out.println();
+
+        }
+
 
     }
 
