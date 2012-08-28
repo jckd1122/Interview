fc8310524b0ae64e817ad772efe7854603bb8a6c
jckd1122 <wmiao1122@gmail.com>
2012-08-28 17:57:45 -0300

    Update leetcode/Java/uniquePathII_Alter.java

--- leetcode/Java/uniquePathII_Alter.java
+++ leetcode/Java/uniquePathII_Alter.java
@@ -2,29 +2,36 @@ public class Solution{
     public int uniquePathsWithObstacles(int[][] obstacleGrid){
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        boolean blocked = false;
-        for(int i = 0; i < m;++i){
-            if(obstacleGrid[i][0] != 1){
-                obstacleGrid[i][0] = 1;
-                if(i == 0)
-                    blocked = true;   //has been changed
-            }
-            else{
-                if(i == 0)
-                    blocked = false;
-                for(int k = i ; k < m;++k)
-                    obstacleGrid[k][0] = 0;
-            }
+        
+        if(obstacleGrid[0][0] == 1){
+            for(int i = 0; i < m;++i)
+                obstacleGrid[i][0] = 0;
+              for(int j = 0; j < n;++j)
+                obstacleGrid[0][j] = 0;
         }
-        if(blocked)
-            obstacleGrid[0][0] = 1;
-        for(int j = 0; j < n;++j){
-            if(obstacleGrid[0][j] != 1)
-                obstacleGrid[0][j] = 1;
-            else{
-                for(int k = j ; k < n;++k)
-                    obstacleGrid[0][k] = 0;
+        else{
+           
+            for(int i = 1; i < m;++i){
+                if(obstacleGrid[i][0] != 1){
+                    obstacleGrid[i][0] = 1;
+                }
+                else{
+                    for(int k = i ; k < m;++k)
+                        obstacleGrid[k][0] = 0;
+                    break;
+                }
             }
+        
+            for(int j = 1; j < n;++j){
+                if(obstacleGrid[0][j] != 1)
+                    obstacleGrid[0][j] = 1;
+                else{
+                    for(int k = j ; k < n;++k)
+                        obstacleGrid[0][k] = 0;
+                    break;
+                }
+            }
+             obstacleGrid[0][0] = 1;
         }
 
         for(int i = 1; i < m;++i){
