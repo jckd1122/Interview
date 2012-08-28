8de14132dfbe57e7d0a4430f9af92c3e36c3450b
jckd1122 <wmiao1122@gmail.com>
2012-08-28 16:49:17 -0300

    Update leetcode/Java/uniquePathI_Alter.java

--- leetcode/Java/uniquePathI_Alter.java
+++ leetcode/Java/uniquePathI_Alter.java
@@ -1,9 +1,9 @@
 public class Solution{
     public int uniquePaths(int m,int n){
-        int[][] arr = int[m][n];
+        int[][] arr = new int[m][n];
         for(int i = 0; i < m;++i)
             arr[i][0] = 1;
-        for(int j = 0; j < j;++j)
+        for(int j = 0; j < n;++j)
             arr[0][j] = 1;
 
         for(int i = 1; i < m;++i){
