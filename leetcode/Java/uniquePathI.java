edd7a80408c2fe946c55a715a76b21ca61a036a5
jckd1122 <wmiao1122@gmail.com>
2012-08-28 15:58:30 -0300

    Update leetcode/Java/uniquePathI.java

--- leetcode/Java/uniquePathI.java
+++ leetcode/Java/uniquePathI.java
@@ -1,27 +1,27 @@
+import java.util.*;
 public class Solution{
     public int uniquePaths(int m,int n){
-        int count = 0;
-        dfs(0,count,0,0,m,n);
-        return count;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        dfs(0,list,0,0,m,n);
+        return list.size();    
+        
 
     }
 
-    void dfs(int depth,int count,int i, int j,int m, int n){
+    void dfs(int depth,ArrayList<Integer> list,int i, int j,int m,int n){
         if(depth == m+n-2){
-            count++;
+            list.add(1);
             return;
         }
 
         if(i+1 < m){
-            dfs(depth+1,count,i+1,j,m,n);
+            dfs(depth+1,list,i+1,j,m,n);
         }
         if(j+1 < n){
-            dfs(depth+1,count,i,j+1,m,n);
+            dfs(depth+1,list,i,j+1,m,n);
         }
-            
-
-
-    }
+        
+     }
 
 
 
