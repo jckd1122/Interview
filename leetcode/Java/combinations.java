753c32189538e1bcadaac536d8f4c3ca528203f6
wenqiang <wmiao1122@gmail.com>
2012-08-29 16:40:18 -0400

    add

---
+++ leetcode/Java/combinations.java
@@ -0,0 +1,30 @@
+public class Solution{
+    public ArrayList<ArrayList<Integer>> combine(int n ,int k){
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> buffer = new ArrayList<Integer>();
+        dfs(list,buffer,0,n,k);
+        return list;
+    }
+
+    void dfs(ArrayList<ArrayList<Integer>> list,ArrayList<Integer> buffer, int depth, int n, int k){
+        if(buffer.size() == k){
+            list.add(buffer);
+            return;
+        }
+
+        for(int i = 1; i <= n;++i){
+            buffer.add(i);
+            dfs(list,buffer,depth+1,n,k);
+            buffer.remove(buffer.size()-1);
+        }
+
+
+    }
+
+
+    
+
+
+
+
+}
