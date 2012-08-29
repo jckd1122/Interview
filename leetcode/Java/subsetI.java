90cad2d4d66e2c3ab6ad0ee0dbbbeae7227f15b8
jckd1122 <wmiao1122@gmail.com>
2012-08-28 22:04:49 -0300

    Update leetcode/Java/subsetI.java

--- leetcode/Java/subsetI.java
+++ leetcode/Java/subsetI.java
@@ -6,35 +6,24 @@ public class Solution{
         if(l == 0)
             return list;
 
-        for(int i = 1; i <=l;++i){
-            ArrayList<Integer> buffer = new ArrayList<Integer>();
-            for(int j = 0; j < l;++j){
-                
-                buffer.add(S[j]);
-                helper(list,buffer,j,i,S);
-                buffer.remove(buffer.size()-1);
-                
-            }
-
-        }
+        
+        ArrayList<Integer> buffer = new ArrayList<Integer>();
+        helper(list,buffer,0,S);
         return list;
 
     }
 
-    void helper(ArrayList<ArrayList<Integer>> list,ArrayList<Integer> buffer,int pos,int length,int[] S){
-        if(buffer.size() == length){
-            list.add(buffer);
-            return;
-        }
+    void helper(ArrayList<ArrayList<Integer>> list,ArrayList<Integer> buffer,int start,int[] S){
+        list.add(new ArrayList<Integer>(buffer));
 
         
-        for(int i = pos; i < S.length;++i){
+        for(int i = start; i < S.length;++i){
             buffer.add(S[i]);
             helper(list,buffer,i+1,length,S);
             buffer.remove(buffer.size()-1);
 
         }
-        return ;
+        
 
     }
 
