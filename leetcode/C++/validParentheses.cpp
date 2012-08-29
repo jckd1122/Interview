fd2039c4d50f0d3ddfb886866c48ebdc777cb030
jckd1122 <wmiao1122@gmail.com>
2012-08-29 01:56:08 -0300

    Update leetcode/C++/validParentheses.cpp

--- leetcode/C++/validParentheses.cpp
+++ leetcode/C++/validParentheses.cpp
@@ -15,6 +15,8 @@ public:
             if(s[i] == '{')
                 c.push_back(i);
             if(s[i] == ')'){
+                if(a.size() == 0)
+                    return false;
                 if(b.size() != 0 && b[b.size()-1] > a[a.size()-1] && i > b[b.size()-1])
                     return false;
                 if(c.size() != 0 && c[c.size()-1] > a[a.size()-1] && i > c[c.size()-1])
@@ -22,6 +24,8 @@ public:
                 a.pop_back();
             }
             if(s[i] == ']'){
+                if(b.size() == 0)
+                    return false;
                 if(a.size() != 0 && a[a.size()-1] > b[b.size()-1] && i > a[a.size()-1])
                     return false;
                 if(c.size() != 0 && c[c.size()-1] > b[b.size()-1] && i > c[c.size()-1])
@@ -29,6 +33,8 @@ public:
                 b.pop_back();
             }
             if(s[i] == '}'){
+                if(c.size() == 0)
+                    return false;
                  if(a.size() != 0 && a[a.size()-1] > c[c.size()-1] && i > a[a.size()-1])
                     return false;
                 if(b.size() != 0 && b[b.size()-1] > c[c.size()-1] && i > b[b.size()-1])
