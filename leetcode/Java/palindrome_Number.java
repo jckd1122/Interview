93d8b0bb4a197d3bd3770bc50848f25b3ed0e488
jckd1122 <wmiao1122@gmail.com>
2012-08-30 18:31:25 -0300

    Update leetcode/Java/palindrome_Number.java

--- leetcode/Java/palindrome_Number.java
+++ leetcode/Java/palindrome_Number.java
@@ -4,16 +4,15 @@ public class Solution{
             return isPalindrome(-x);
         
         int remain = 0;
-        int tail = 0;
+        int num = x;
+        //int tail = 0;
         int count = 0;
-        while(x/10 > 0){
-            tail = x%10;
+        while(x > 0){
             x /= 10;
-            remain = tail*(int)Math.pow(10,count)+remain;
             count++;
         }
+        remain = num-x*Math.pow(10,count);
 
-        count--;
         while(remain >= 10){
             if(x != remain%10)
                 return false;
