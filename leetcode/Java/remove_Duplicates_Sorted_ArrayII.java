fac8b15a8bdd8ea07adbd50500e2a569fb7e4fe4
wenqiang <wmiao1122@gmail.com>
2012-08-31 09:21:43 -0400

    add

---
+++ leetcode/Java/remove_Duplicates_Sorted_ArrayII.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        int n = A.length;
+        if(n == 0)
+            return 0;
+        
+        int count = 0;    
+        int head = 0;
+        for(int i = 0; i < n;++i){
+            if(A[head] == A[i]){
+                count++;
+                continue;
+            }
+            else{
+                if(count == 1)
+                    head++;
+                else if(count >=2){
+                    A[head+1] = A[head];
+                    head = head+2;
+                }
+                A[head] = A[i];
+                count = 1;
+            }
+        }
+        return head+1;
+    }
+}
