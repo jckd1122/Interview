1a607ac17cc58ac8277851aec9b5028dd8fe7452
jckd1122 <wmiao1122@gmail.com>
2012-08-31 10:37:36 -0300

    Update leetcode/Java/remove_Duplicates_Sorted_ArrayII.java

--- leetcode/Java/remove_Duplicates_Sorted_ArrayII.java
+++ leetcode/Java/remove_Duplicates_Sorted_ArrayII.java
@@ -9,8 +9,10 @@ public class Solution {
         for(int i = 0; i < n;++i){
             if(A[head] == A[i]){
                 count++;
-                if(i == n-1 %% head < n-1)
+                if(i == n-1 && head < n-1){
                     A[head+1] = A[i];
+                    head++;
+                }
                 continue;
             }
             else{
