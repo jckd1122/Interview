5a301f54834953371f21e58e3a0a0607c45516b8
jckd1122 <wmiao1122@gmail.com>
2012-08-31 00:15:35 -0300

    Update leetcode/Java/palindrome_Number.java

--- leetcode/Java/palindrome_Number.java
+++ leetcode/Java/palindrome_Number.java
@@ -1,25 +1,22 @@
 public class Solution{
     public boolean isPalindrome(int x){
-        if(x <= 0)
-            return false;
+        if(x < 0)
+            return false;       
         
-        int len = 1;
+        int len = 0;
         int head = 0;
         int tail = 0;
-        while(x/(int)Math.pow(10,len) > 0){
+        while((int)(x/Math.pow(10,len)) > 0){
             len++;
         }
         
-
         for(int i = 0; i < len/2;++i){
-            head = x/(int)Math.pow(10,len-i);
+            head = (x/(int)Math.pow(10,len-i-1))%10;
             tail = (x/(int)Math.pow(10,i))%10;
             if(head != tail)
                 return false;
         }
         return true;
     }
-
-
 
 }
