2f14d7ed1218da64c3100b593aa297b69e25c9ce
jckd1122 <wmiao1122@gmail.com>
2012-09-04 17:31:15 -0300

    Update leetcode/Java/interleaving_String.java

--- leetcode/Java/interleaving_String.java
+++ leetcode/Java/interleaving_String.java
@@ -1,10 +1,19 @@
 public class Solution{
     public boolean isInterleave(String s1,String s2,String s3){
-        if(s1.length() == 0 && s2 == s3)
-            return true;
-        if(s2.length() == 0 && s1 == s3)
-            return true;
-
+        if(s1.length() == 0){
+            if(s2 == s3)
+                return true;
+            else
+                return false;
+        }
+    
+        if(s2.length() == 0){
+            if(s1 == s2)
+                return true;
+            else
+                return false;
+        }
+        
         if(s3.charAt(0) == s1.charAt(0))
             return isInterleave(s1.substring(1),s2,s3.substring(1));
         else if(s3.charAt(0) == s2.charAt(0))
