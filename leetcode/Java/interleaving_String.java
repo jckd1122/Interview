dc1256b069d764c5fe46fa026518d45a5bda1eef
jckd1122 <wmiao1122@gmail.com>
2012-09-04 17:40:05 -0300

    Update leetcode/Java/interleaving_String.java

--- leetcode/Java/interleaving_String.java
+++ leetcode/Java/interleaving_String.java
@@ -14,12 +14,17 @@ public class Solution{
                 return false;
         }
         
-        if(s3.charAt(0) == s1.charAt(0))
-            return isInterleave(s1.substring(1),s2,s3.substring(1));
-        else if(s3.charAt(0) == s2.charAt(0))
-            return isInterleave(s1,s2.substring(1),s3.substring(1));
-        else
-            return false;
+        if(s1.charAt(0) != s2.charAt(0)){
+            if(s3.charAt(0) == s1.charAt(0))
+                return isInterleave(s1.substring(1),s2,s3.substring(1));
+            else if(s3.charAt(0) == s2.charAt(0))
+                return isInterleave(s1,s2.substring(1),s3.substring(1));
+        }
+        else{
+            return isInterleave(s1.substring(1),s2,s3.substring(1)) || isInterleave(s1,s2.substring(1),s3.substring(1));
+        }
+        
+        return false;
     }
 
 
