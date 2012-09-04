ab49da84e85419406e0e83da2a475a0c5b350893
jckd1122 <wmiao1122@gmail.com>
2012-09-04 18:15:54 -0300

    Update leetcode/Java/interleaving_String.java

--- leetcode/Java/interleaving_String.java
+++ leetcode/Java/interleaving_String.java
@@ -2,12 +2,12 @@ public class Solution{
     public boolean isInterleave(String s1,String s2,String s3){
         
         
-        int[][] table = new table[s1.length()][s2.length()];
-        return (helper(s1,s2,s3,0,0,table) == 1)? true:false;
+        int[][] table = new int[s1.length()][s2.length()];
+        return (helper(s1,s2,s3,table,0,0) == 1)? true:false;
 
     }
 
-    int helper(String s1,String s2,String s3,int i, int j,int[][] table){
+    int helper(String s1,String s2,String s3,int[][] table,int i,int j){
         if(i == s1.length()){
             if(s2.substring(j).equals(s3.substring(i+j)))
                 return 1;
@@ -33,7 +33,7 @@ public class Solution{
         }
         else{
             if(s3.charAt(i+j) == s1.charAt(i))
-                table[i][j] = helper(s1.substring(i+1),s2,s3.substring(i+j)) || isInterleave(s1,s2.substring(j+1),s3.substring(i+j));
+                table[i][j] = helper(s1.substring(i+1),s2,s3.substring(i+j),i+1,j) || isInterleave(s1,s2.substring(j+1),s3.substring(i+j),i,j+1);
             else
                 table[i][j] = -1;
         }
