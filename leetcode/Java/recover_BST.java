dc501e2c71203f990bf0d906a93a4b787b1225fd
wenqiang <wmiao1122@gmail.com>
2012-09-05 17:37:16 -0400

    add

--- leetcode/Java/recover_BST.java
+++ leetcode/Java/recover_BST.java
@@ -1,47 +1,25 @@
 public class Solution{
     public void recoverTree(TreeNode root){
-        if(root == null)
-            return;
-        TreeNode l = root.left;
-        TreeNode r = root.right;
-        if(l != null && r != null){
-            if(root.val > l.val){
-                recoverTree(l);
-            if(root.val < r.val)
-                recoverTree(r);
-            if(root.val < l.val)
-                modify(root,l);
-            else 
-                modify(root,r);
-                
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        inOrder(root,list);
+
+        for(int i = 0; i < list.size();++i){
+
 
         }
-    }
+        
 
-
-    void modify(TreeNode root,Tree subRoot){
-        if(root.val < subRoot.val){  //left
-           TreeNode tmp = subRoot;
-           while(tmp.left != null || tmp.right != null){
-               
-               if(root.val < tmp.right.val && root.val > tmp.left.val){
-                    swap(tmp,root);
-                }
-               else if(root.val < tmp.left.val){
-                    tmp = tmp.left;
-               }
-               else
-                   tmp = tmp.right;
-            }
-            
-        }
-
-        if(
 
     }
 
+    void inOrder(TreeNode root, ArrayList<Integer> list){
+        if(root.left != null)
+            inOrder(root.left,list);
+        list.add(root.val);
+        if(root.right != null)
+            inOrder(root.right);
 
 
-
+    }
 
 }
