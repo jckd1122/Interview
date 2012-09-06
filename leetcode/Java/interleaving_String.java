fa329e9510328a94d6ccbdc77b501625973c5650
jckd1122 <wmiao1122@gmail.com>
2012-09-06 00:30:54 -0300

    Update leetcode/Java/interleaving_String.java

--- leetcode/Java/interleaving_String.java
+++ leetcode/Java/interleaving_String.java
@@ -1,10 +1,8 @@
+//top-down approach
 public class Solution{
     public boolean isInterleave(String s1,String s2,String s3){
-        
-        
         int[][] table = new int[s1.length()][s2.length()];
         return (helper(s1,s2,s3,table,0,0) == 1)? true:false;
-
     }
 
     int helper(String s1,String s2,String s3,int[][] table,int i,int j){
@@ -14,7 +12,6 @@ public class Solution{
             else
                 return -1;
         }
-    
         if(j == s2.length()){
             if(s1.substring(i).equals(s3.substring(i+j)))
                 return 1;
@@ -24,29 +21,14 @@ public class Solution{
 
         if(table[i][j] != 0)
             return table[i][j];
-
-        if(s1.charAt(i) != s2.charAt(j)){
-            if(s3.charAt(i+j) == s1.charAt(i))
-                table[i][j] = helper(s1,s2,s3,table,i+1,j);
-            else if(s3.charAt(i+j) == s2.charAt(j))
-                table[i][j] = helper(s1,s2,s3,table,i,j+1);
-        }
         
-        else{
-            if(s3.charAt(i+j) != s2.charAt(j)){
-                table[i][j] = -1;
-               
-            }
-                
-            else if(helper(s1,s2,s3,table,i+1,j) == 1 || helper(s1,s2,s3,table,i,j+1) == 1)
-                table[i][j] = 1;
-                
-        }
-      
-        
-        return table[i][j];
-        
-        
+        if(i < s1.length() && s3.charAt(i+j) == s1.charAt(i) && helper(s1,s2,s3,table,i+1,j) == 1)
+            table[i][j] = 1;
+        else if(j < s2.length() && s3.charAt(i+j) == s2.charAt(j) && helper(s1,s2,s3,table,i,j+1) == 1)
+            table[i][j] = 1;
+        else
+            table[i][j] = -1;
+       
+       return table[i][j];
     }
-
 }
