53ce86fb6d2ead5f9e5e4ea6a76c5f1695948eac
jckd1122 <wmiao1122@gmail.com>
2012-09-06 15:48:02 -0300

    Update leetcode/Java/interleaving_String_Alter.java

--- leetcode/Java/interleaving_String_Alter.java
+++ leetcode/Java/interleaving_String_Alter.java
@@ -26,6 +26,34 @@ public class Solution{
 
 
     }
+    
+    public boolean isInterleave(String s1,String s2,String s3){
+        if(s1 == null || s2 == null || s3 == null)
+            return false;
+
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int l3 = s3.length();
+        if(l1+l2 != l3)
+            return false;
+
+        boolean[][] table = new boolean[l1+1][l2+1];
+        table[0][0] = true;
+        
+
+        for(int i = 0; i <= l1;++i){
+            for(int j = 0; j <= l2; ++j){
+                if(i > 0 && s1.charAt(i-1) == s3.charAt(i+j-1) && table[i-1][j])
+                    table[i][j] = true;
+                if(j > 0 && s2.charAt(j-1) == s3.charAt(i+j-1) && table[i][j-1])
+                    table[i][j] = true;
+            }
+        }
+
+        return table[l1][l2];
+
+
+    }
 
 
 
