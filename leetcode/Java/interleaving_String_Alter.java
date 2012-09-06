f23a1a388126a52d9698dec662c7164c0676a30d
jckd1122 <wmiao1122@gmail.com>
2012-09-06 15:14:22 -0300

    Update leetcode/Java/interleaving_String_Alter.java

--- leetcode/Java/interleaving_String_Alter.java
+++ leetcode/Java/interleaving_String_Alter.java
@@ -10,18 +10,19 @@ public class Solution{
             return false;
 
         boolean[][] table = new boolean[l1+1][l2+1];
+        table[0][0] = true;
         
 
-        for(int i = 0; i < l1+1;++i){
-            for(int j = 0; j < l2+1; ++j){
-                if(i > 0 && s1.charAt(i) == s3.charAt(i+j) && table[i-1][j])
+        for(int i = 0; i <= l1;++i){
+            for(int j = 0; j <= l2; ++j){
+                if(i > 0 && s1.charAt(i-1) == s3.charAt(i+j-1) && table[i-1][j])
                     table[i][j] = true;
-                if(j > 0 && s2.charAt(j) == s3.charAt(i+j) && table[i][j-1])
+                if(j > 0 && s2.charAt(j-1) == s3.charAt(i+j-1) && table[i][j-1])
                     table[i][j] = true;
             }
         }
 
-        return table[l1-1][l2-1];
+        return table[l1][l2];
 
 
     }
