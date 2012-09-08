29873b2339f753c69330bdd625d067b520cfff24
jckd1122 <wmiao1122@gmail.com>
2012-09-08 02:45:54 -0300

    Update leetcode/Java/same_Tree.java

--- leetcode/Java/same_Tree.java
+++ leetcode/Java/same_Tree.java
@@ -13,9 +13,6 @@ public class Solution {
             return true;
         if(p == null || q == null)
             return false;
-        if(p.val == q.val)
-            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-        else
-            return false;
+        return (p.val == q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
