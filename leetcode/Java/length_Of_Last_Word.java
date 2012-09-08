47b75da30412533960d2c044b360aebcedc42259
jckd1122 <wmiao1122@gmail.com>
2012-09-08 02:58:17 -0300

    Update leetcode/Java/length_Of_Last_Word.java

--- leetcode/Java/length_Of_Last_Word.java
+++ leetcode/Java/length_Of_Last_Word.java
@@ -1,1 +1,15 @@
-idasdhja
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int l = s.length();
+        //if(l == 0)
+        //    return 0;
+        
+        int tail = l-1;
+        while(tail >= 0 && s.charAt(tail) == ' ')
+            tail--;
+        int head = tail;
+        while(head >= 0 && s.charAt(head) != ' ')
+            head--;
+        return tail-head;
+    }
+}
