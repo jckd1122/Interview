8f579d5b83c082e570f86b8894cfbdf854f2eb18
wenqiang <wmiao1122@gmail.com>
2012-09-10 17:39:32 -0400

    add

---
+++ cup150/chapter3/sort_Stack.java
@@ -0,0 +1,24 @@
+public class sort_Stack{
+    
+    void sort(Stack<Intger> s1){
+        Stack<Integer> s2 = new Stack<Integer>();
+        int tm1 = 0;
+        int tmp2 = ;
+        while(!s1.empty()){
+            if(tmp2 < tmp1){
+                int tmp1 = s2.pop();
+                int tmp2 = s1.pop();
+                s1.push(tmp1);
+                continue;
+            }
+            
+
+        }
+
+
+
+    }
+
+
+
+}
