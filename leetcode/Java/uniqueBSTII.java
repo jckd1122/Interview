a2bc6dce7310e4075d1ec7ea7944f675939505b2
jckd1122 <wmiao1122@gmail.com>
2012-09-12 00:17:12 -0300

    Update leetcode/Java/uniqueBSTII.java

--- leetcode/Java/uniqueBSTII.java
+++ leetcode/Java/uniqueBSTII.java
@@ -2,28 +2,34 @@ import java.util.*;
 public class Solution{
     public ArrayList<TreeNode> generateTrees(int n){
         ArrayList<TreeNode> list = new ArrayList<TreeNode>();
-        generationHelper(list,1,n,0);
-        return list;
+        return generationHelper(list,1,n,0);
+       
 
     }
 
-    public TreeNode generationHelper(ArrayList<TreeNode> list,int start, int end,int depth){
+    public ArrayList<TreeNode> generationHelper(ArrayList<TreeNode> list,int start, int end,int depth){
         if(start > end)
             return null;
 
         TreeNode root = null;
         for(int i = start; i <= end;++i){
             root = new TreeNode(i);
-            root.left = generationHelper(list,start, i-1,depth+1);
-            root.right = generationHelper(list,i+1,end,depth+1);
-            if(depth == 0)
-                list.add(root);
+            if(generationHelper(list,start, i-1,depth+1) == null)
+                root.left = null;
+            if(generationHelper(list,i+1, end,depth+1) == null)
+                root.right = null;
+            for(TreeNode ele :generationHelper(list,start, i-1,depth+1))
+                    root.left = ele;
+            for(TreeNode ele :generationHelper(list,i+1, end,depth+1))
+                root.right = ele;
+            list.add(root);    
             //if(root.left == null && root.right == null)
             //    list.add(root);
                 
         
         }
-        return root;
+        
+        return list;
     }
 
 
