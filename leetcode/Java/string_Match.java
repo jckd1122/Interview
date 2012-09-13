0d61c69da860f090324f96790064ee8bbf29ecbb
wmiao1122 <wenqiangmiao@Wenqiang-Miaos-MacBook-Pro.local>
2012-09-13 00:20:10 -0400

    add

--- leetcode/Java/string_Match.java
+++ leetcode/Java/string_Match.java
@@ -7,15 +7,15 @@ public class Solution{
         if(l1 == 0)
             return (l2 == 0);
         
-        if(j < l2-1 && p.charAt(j+1) != '*')
-            return (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') && isMatch(s.substring(i+1),p.substring(j+1));
-        
-        while((i < l1 && s.charAt(i) == p.charAt(j)) || (i < l1 && p.charAt(j) == '.')){
-            if(isMatch(s.substring(i),p.substring(j+2)))
-                return true;
-            i++;
-        }        
-        return isMatch(s.substring(i),p.substring(j+2));
+        //if(j < l2-1 && p.charAt(j+1) != '*')
+        //    return (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') && isMatch(s.substring(i+1),p.substring(j+1));
+        //
+        //while((i < l1 && s.charAt(i) == p.charAt(j)) || (i < l1 && p.charAt(j) == '.')){
+        //    if(isMatch(s.substring(i),p.substring(j+2)))
+        //        return true;
+        //    i++;
+        //}        
+        //return isMatch(s.substring(i),p.substring(j+2));
 
     }
 
