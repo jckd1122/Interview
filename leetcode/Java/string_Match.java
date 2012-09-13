12a230142567d915ea282ec49484b67ad49b8e68
jckd1122 <wmiao1122@gmail.com>
2012-09-13 01:19:17 -0300

    Update leetcode/Java/string_Match.java

--- leetcode/Java/string_Match.java
+++ leetcode/Java/string_Match.java
@@ -2,45 +2,20 @@ public class Solution{
     public boolean isMatch(String s,String p){
         int l1 = s.length();
         int l2 = p.length();
-        if(l1 == 0)
-            return false;
         int i = 0;
         int j = 0;
-        while(i < l1 && j < l2){
-            if(s.charAt(i) != p.charAt(j) && p.charAt(j) != '.'){
-                if(j < l2-1 && p.charAt(j+1) == '*'){
-                    j = j+2;
-                    continue;
-                }
-                else
-                    return false;
-                
-                
-            }
-            else {
-                if(j == l2-1)
-                    return (i == l1-1)? true:false;
-                if(p.charAt(j+1) == '*'){
-                    char c = s.charAt(i);
-                    j = j+2;
-                    while(i < l1 && s.charAt(i) == c)
-                        i++;
-                    while(j < l2 && p.charAt(j) == c)
-                        j++;
-                    continue;
-                }
-                else{
-                    i++;
-                    j++;
-                }
-
-            }
-            
-
-
-        }
-        return (i >= l1 && j >= l2)? true:false;
-
+        if(l1 == 0)
+            return (l2 == 0);
+        
+        if(j < l2-1 && p.charAt(j+1) != '*')
+            return (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') && isMatch(s.substring(i+1),p.substring(j+1));
+        
+        while((i < l1 && s.charAt(i) == p.charAt(j)) || (i < l1 && p.charAt(j) == '.')){
+            if(isMatch(s.substring(i),p.substring(j+2)))
+                return true;
+            i++;
+        }        
+        return isMatch(s.substring(i),p.substring(j+2));
 
     }
 
