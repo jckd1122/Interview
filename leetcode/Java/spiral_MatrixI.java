040d94db9907d8140b6760397370663f07736c7c
jckd1122 <wmiao1122@gmail.com>
2012-09-23 01:23:34 -0300

    Update leetcode/Java/spiral_MatrixI.java

--- leetcode/Java/spiral_MatrixI.java
+++ leetcode/Java/spiral_MatrixI.java
@@ -14,13 +14,19 @@ public class Solution{
                     list.add(matrix[s][i]);
                 return list;
             }
+            if(m-2*i == 1){
+                for(int s = i; s < n-i;++s)
+                    list.add(matrix[i][s]);
+                return list;
+            }
+            
             for(int j = i; j < n-i-1;++j)
                 list.add(matrix[i][j]);
             for(int k = i; k < m-i-1;++k)
                 list.add(matrix[k][n-i-1]);
-            for(int p = n-i-1; p > i;++p)
+            for(int p = n-i-1; p > i;--p)
                 list.add(matrix[m-i-1][p]);
-            for(int q = m-i-1; q > i;++q)
+            for(int q = m-i-1; q > i;--q)
                 list.add(matrix[q][i]);
 
             
@@ -31,3 +37,5 @@ public class Solution{
 
     
 
+
+}
