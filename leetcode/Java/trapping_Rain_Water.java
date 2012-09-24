8ffbcc9ee0cd469b4e0b95f17c89845bc0c95d17
wenqiang <wmiao1122@gmail.com>
2012-09-24 16:23:03 -0400

    add

---
+++ leetcode/Java/trapping_Rain_Water.java
@@ -0,0 +1,61 @@
+public class Solution{
+    public int trap(int[] A){
+        int l = A.length;
+        int sum = 0;
+        int i = 0;
+        int left = 0;
+        int right = 0;
+        int prev = 0;
+        boolean isLeft = false;
+        boolean isRight = false;
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        while(i < l){
+            //determine decrease
+            while(A[i] < prev){
+                list1.add(A[i]);
+                prev = A[i];
+                i++;
+            }
+            
+            while(A[i] > prev){
+                list2.add(A[i]);
+                prev = A[i];
+                i++;
+            }
+            
+            if(A[i] == prev{)
+                right = i-1;
+                sum += cal(list1,list2,left,right,A);
+                left = i;
+            }
+            else{
+                right = i-1;
+                sum += cal(list1,list2,left,right,A);
+                left = i-1;
+            }
+
+            //determine increase
+
+        }
+        return sum;
+
+
+    }
+
+
+    int cal(ArrayList<Integer> lis1,ArrayList<Integer< list2,int left,int right,int[] A){
+        int sum = 0;
+        int height = (left < right)? left:right;
+        sum += right-left-1;
+        for(int ele:list1)
+            sum -= ele;
+        for(int ele:list2)
+            sum -= ele;
+        return sum;
+
+
+
+    }
+
+}
