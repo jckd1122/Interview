f9813e336434869320ace3de9da41a4a09f441d0
jckd1122 <wmiao1122@gmail.com>
2012-09-25 10:54:19 -0300

    Update leetcode/Java/trapping_Rain_Water.java

--- leetcode/Java/trapping_Rain_Water.java
+++ leetcode/Java/trapping_Rain_Water.java
@@ -13,13 +13,13 @@ public class Solution{
         while(i < l){
             //determine decrease
             while(i < l && A[i] <= prev ){
-                list1.add(A[i]);
+                list1.add(i);
                 prev = A[i];
                 i++;
             }
             
             while(i < l && A[i] >= prev ){
-                list2.add(A[i]);
+                list2.add(i);
                 prev = A[i];
                 i++;
             }
@@ -42,11 +42,28 @@ public class Solution{
     int cal(ArrayList<Integer> list1,ArrayList<Integer> list2,int left,int right,int[] A){
         int sum = 0;
         int height = (A[left] < A[right])? A[left]:A[right];
+        if(A[left] < A[right]){
+            for(int ele : list2){
+                if(A[ele] >= A[left] ){
+                    right = ele;
+                    break;
+                }
+            }
+        }
+        else{
+            for(int ele : list1){
+                if(A[ele] < A[left] ){
+                    left = ele-1;
+                    break;
+                }
+            }
+        }
+        //int height = (A[left] < A[right])? A[left]:A[right];
         sum +=(right-left-1)*height;
         for(int ele:list1)
-            sum -= ele;
+            sum -= A[ele];
         for(int ele:list2)
-            sum -= ele;
+            sum -= A[ele];
         list1.clear();
         list2.clear();
         return sum;
