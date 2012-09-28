5ea3cd9b16c2f7f3f774c95ad3a334fd5a2ea127
jckd1122 <wmiao1122@gmail.com>
2012-09-28 20:03:36 -0300

    Update leetcode/Java/zigzag_Conversion.java

--- leetcode/Java/zigzag_Conversion.java
+++ leetcode/Java/zigzag_Conversion.java
@@ -4,21 +4,57 @@ public class Solution {
         // DO NOT write main() function
         String p = "";
         String q = s;
-        char[][] table = new char[nRows][s.length()/nRows+1];
+        char[][] table = new char[nRows][s.length()/2+2];
+        for(int i = 0; i < nRows;++i){
+            for(int j = 0; j < s.length()/2+2;++j){
+                table[i][j] = ' ';       
+            }
+        }
+        
         if(nRows == 1 || nRows >= s.length())
             return s;
         if(nRows == 2){
-            for(int j = 0; j < s.length()/nRows+1;++j){
+            for(int j = 0; j < (s.length()/2)+2;++j){
                 for(int i = 0; i < nRows;++i){
+                    
                     table[i][j] = q.charAt(0);
-                    q = q.substring(1);
+                    if(q.length() >= 2)
+                        q = q.substring(1);
+                    
                 }
             }
         }
         
+        if(nRows >= 3){
+            int j = 0;
+            while(j < (s.length()/2)+2){
+                if(j %2 == 0){
+                    if(j == 0){
+                        for(int i = 0; i < nRows;++i){
+                            table[i][j] = q.charAt(0);
+                            if(q.length() >= 2)
+                                q = q.substring(1);
+                        }
+                    }
+                    else{
+                        for(int i = nRows-3; i < nRows;++i){
+                            table[i][j] = q.charAt(0);
+                            if(q.length() >= 2)
+                                q = q.substring(1);
+                        }
+                    }
+                }
+                else{
+                   table[nRows-2][j] = q.charAt(0);
+                   if(q.length() >= 2)
+                       q = q.substring(1); 
+                }
+                j++;
+            }
+        }
         
         for(int i = 0; i < nRows;++i){
-            for(int j = 0; j < s.length()/nRows+1;++j){
+            for(int j = 0; j < s.length()/2+2;++j){
                 if(table[i][j] != ' ')
                     q = q+table[i][j];
             }
