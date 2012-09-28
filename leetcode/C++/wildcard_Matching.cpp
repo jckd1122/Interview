0d55e4190888efc9381e708e4d170341307a508e
jckd1122 <wmiao1122@gmail.com>
2012-09-28 11:26:36 -0300

    Update leetcode/C++/wildcard_Matching.cpp

--- leetcode/C++/wildcard_Matching.cpp
+++ leetcode/C++/wildcard_Matching.cpp
@@ -21,6 +21,30 @@ public:
             return parse_one(s,p) && isMatch(s+1,p+1);
 
     }
+    
+    //using DP 
+    bool isMatch(const char *s,const char *p){
+        bool[][] table = new int[][];
+        if(*p == '\0')
+            return (*p == *s);
+
+        if(p[0] == '*'){
+            while(*s != '\0'){
+                if(isMatch(s,p+1))
+                    return true;
+                s++;
+            }
+            while(*p != '\0'){
+                if(*p != '*')
+                    return false;
+                p++;
+            }
+            return true;
+        }
+        else
+            return parse_one(s,p) && isMatch(s+1,p+1);
+
+    }
 
    bool parse_one(const char *s,const char *p){
         if(*s != '\0' && (*p == '?' || *p == *s))
