d192a83b55d5a762e1efaa1bf42fc31c571c1355
wenqiang <wmiao1122@gmail.com>
2012-10-01 15:58:15 -0400

    add

--- leetcode/Java/depth_Of_BinaryTree.java
+++ leetcode/Java/depth_Of_BinaryTree.java
@@ -5,5 +5,35 @@ public class Solution{
         return Math.max(1+maxHeight(root.left),1+maxHeight(root.right));
     }
 
+    public int maxHeight(TreeNode root){
+        if(root == null)
+            return 0;
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        root.val = 1;
+        s.push(root);
+        int height = 0;
+        int maxHeight = 0;
+        while(!s.empty()){
+            TreeNode node = s.pop();
+            height = node.val;
+            if(maxHeight < height)
+                maxHeight = height;
+            if(node.right != null){
+                node.right.val = height+1;
+                s.push(node.right);
+            }
+            if(node.left != null){
+                node.left.val = height+1;
+                s.push(node.left);
+            }
+
+
+        }
+        return maxHeight;
+
+
+
+    }
+
 
 }
