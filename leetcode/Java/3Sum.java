0ba53ce89c77a017de295b54860f67c84cee4d56
jckd1122 <wmiao1122@gmail.com>
2012-10-02 16:42:29 -0300

    Update leetcode/Java/3Sum.java

--- leetcode/Java/3Sum.java
+++ leetcode/Java/3Sum.java
@@ -1,55 +1,32 @@
 import java.util.*;
+
 public class Solution {
      public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
         Arrays.sort(num);
-        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        Set<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>();
         int Sum = 0;
          for(int i = 0; i < num.length-2;++i){
             Sum = -num[i];
             int j = i+1;
             int k = num.length-1;
-            int prevLo = -1000;
-            int prevHi = 1000;
             ArrayList<Integer> ele = new ArrayList<Integer>(); 
             while(j < k){
-                if(num[j] == prevLo)
-                    j++;
-                   
-                if(num[k] == prevHi)
-                    k++;
-                  
-                
-                if(num[j]+num[k] == Sum){
+               if(num[j]+num[k] == Sum){
                     ele.add(num[i]);
                     ele.add(num[j]);
                     ele.add(num[k]);
-                    list.add(new ArrayList<Integer>(ele));
+                    set.add(new ArrayList<Integer>(ele));
                     ele.clear();
                     k--;
                     j++;
-                    
                 }
-                else if(num[j]+num[k] > Sum){
-                   
-                    k--;
-                }
-                else{
-                    
+                else if(num[j]+num[k] > Sum)
+                   k--;
+                else
                     j++;
-                }  
-                if(j < num.length)
-                    prevLo = num[j-1];
-                if(k > i+1)
-                    prevHi= num[k+1]; 
-                
             }
-            
-           
-            
         }
-               
-            
-        return list;
+        return new ArrayList<ArrayList<Integer> >(set);
         
     }
 }
