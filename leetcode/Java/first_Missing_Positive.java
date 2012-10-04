f72da06df70f93ff44384d4293ef2dccbbc94566
jckd1122 <wmiao1122@gmail.com>
2012-10-04 00:35:59 -0300

    Update leetcode/Java/first_Missing_Positive.java

--- leetcode/Java/first_Missing_Positive.java
+++ leetcode/Java/first_Missing_Positive.java
@@ -1,1 +1,24 @@
-ljdljl
+public class Solution {
+    public int firstMissingPositive(int[] A) {
+       int n = A.length;
+       if(n == 0)
+           return 1;
+       int[] arr = new int[n+1];
+       for(int i = 0; i < A.length;++i){
+           if(A[i] > 0 && A[i] < n  && A[i] != arr[A[i]]){
+               arr[A[i]] = A[i];
+           } 
+       
+       }
+       
+       for(int i = 1;i < arr.length;++i){
+           if(arr[i] != i)
+               return i;
+       }
+       return n+1;
+        
+    }
+    
+   
+ 
+}
