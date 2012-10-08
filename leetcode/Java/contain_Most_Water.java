286bf08c2ed33c5c8557be285f6fe488fc6f21d1
jckd1122 <wmiao1122@gmail.com>
2012-10-08 12:20:52 -0300

    Update leetcode/Java/contain_Most_Water.java

--- leetcode/Java/contain_Most_Water.java
+++ leetcode/Java/contain_Most_Water.java
@@ -1,2 +1,21 @@
-lkajdlsja:x
+public class Solution {
+    public static int  maxArea(int[] height){
+        int h,width,area,areaMax = 0;
+        int l = 0;
+        int r = height.length-1;
+        while(l < r){
+             h = Math.min(height[l],height[r]);
+             width = r-l;
+             area = h*width;
+             if(area > areaMax)
+                 areaMax = area;
+            if(height[l] < height[r])
+                l++;
+            else
+                r--;    
+        }
+        return areaMax;
+    }
+}
+
 
