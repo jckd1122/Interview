7b091fffba60e63062b93888b60fd7f1f673bfbf
jckd1122 <wmiao1122@gmail.com>
2012-10-09 18:14:16 -0300

    Update leetcode/Java/valid_Sudoku.java

--- leetcode/Java/valid_Sudoku.java
+++ leetcode/Java/valid_Sudoku.java
@@ -1,1 +1,63 @@
-kljdjalk
+import java.util.*;
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[] table = new boolean[10];
+        int m = board.length;
+        int n = board[0].length;
+        if(m != 9 || n != 9) return false;
+        
+        for(int i = 0; i < m;++i){
+            Arrays.fill(table,false);
+            for(int j = 0; j < n;++j){
+                if(board[i][j] != '.'){
+                    if(table[board[i][j] -'0'])
+                        return false;
+                    else
+                        table[board[i][j]-'0'] = true;
+                }
+            }
+       }
+       
+        for(int i = 0; i < n;++i){
+            Arrays.fill(table,false);
+            for(int j = 0; j < m;++j){
+                if(board[j][i] != '.'){
+                    if(table[board[j][i] -'0'])
+                        return false;
+                    else
+                        table[board[j][i]-'0'] = true;
+                }
+            }
+       }
+       
+       for(int i = 0; i < 3;++i){
+           for(int j = 0; j < 3;++j){
+                Arrays.fill(table,false);
+                for(int x = 0; x < 3;++x){
+                    for(int y = 0; y < 3;++y){
+                        if(board[3*i+x][3*j+y] != '.'){
+                            if(table[board[i][j] -'0'])
+                                return false;
+                            else
+                                table[board[3*i+x][3*j+y]-'0'] = true;
+                        }
+                    }
+                    
+               }
+                
+                
+           }
+           
+      }
+      
+      return true;
+       
+       
+       
+       
+        
+        
+    }
+}
+
