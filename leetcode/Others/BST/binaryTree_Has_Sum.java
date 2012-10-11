8c8cdcdcf458aadeaa1cf370e796ec4af34d0e5c
wenqiang <wmiao1122@gmail.com>
2012-10-11 16:06:41 -0400

    add

--- leetcode/Others/BST/binaryTree_Has_Sum.java
+++ leetcode/Others/BST/binaryTree_Has_Sum.java
@@ -2,11 +2,18 @@ import java.util.*;
 
 public class binaryTree_Has_Sum{
     public static boolean hasSum(Node root,int sum){
-        if(root == null)
-            return (sum == 0);
+        if(root.left == null && root.right == null)
+            return root.val == sum;
         
+        
+        if(root.left != null)
+            return hasSum(root.left,sum-root.val);
+        if(root.right != null)
+            return hasSum(root.right,sum-root.val);
         
         return hasSum(root.left,sum-root.val) || hasSum(root.right,sum-root.val);
+
+        
     }
     
     
@@ -22,36 +29,42 @@ public class binaryTree_Has_Sum{
     public static void printPath_Helper(Node root,int sum,ArrayList<ArrayList<Integer>> list,ArrayList<Integer> path){
      
        
-       if(root == null){
-           if(sum == 0)
+       if(root.left  == null && root.right == null){
+           if(sum == root.val){
+               path.add(root.val);
                list.add(new ArrayList<Integer>(path));
+           }
            return; 
        }
         
         
         path.add(root.val);
-        printPath_Helper(root.left,sum-root.val,list,path);
+        if(root.left != null)
+            printPath_Helper(root.left,sum-root.val,list,path);
+        
+        if(root.right != null)
+            printPath_Helper(root.right,sum-root.val,list,path);
+        
         path.remove(path.size()-1);
-        
-        
-        
-        path.add(root.val);
-        printPath_Helper(root.right,sum-root.val,list,path);
+
         
         
     }
 
     public static void main(String[] args){
-       int[] arr = {1,2,3,4,5,6,7,8,9};
-       Node root = Node.arrayToBST(arr,0,8);
-       System.out.println(hasSum(root,14));
-       ArrayList<ArrayList<Integer>> list = printPath(root,14);
+       int[] arr = {2,3,4};
+       Node root = Node.arrayToBST(arr,0,2);
+       Node tree = new Node(1);
+       tree.right = new Node(2);
+
+       //System.out.println(hasSum(tree,3));
+       ArrayList<ArrayList<Integer>> list = printPath(root,7);
        for(ArrayList<Integer> ele : list){
            for(int num:ele)
                System.out.print(num+" ");
            System.out.println();
-
        }
+
 
     }
 
