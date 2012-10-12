984366e9901289c35282d058a95e45f376dd2bcf
jckd1122 <wmiao1122@gmail.com>
2012-10-12 12:49:06 -0300

    Update cup150/chapter8/way_Of_Change.java

--- cup150/chapter8/way_Of_Change.java
+++ cup150/chapter8/way_Of_Change.java
@@ -1,28 +1,38 @@
 //8.7
 
 public class way_Of_Change{
-    public static int getChange(int n){
+    public static int getChange(int n,int denom){
         
         int count = 0;
-        if(n >= 25)
-            count += 1+getChange(n-25);
-        else if(n >= 10)
-            count += 1+getChange(n-10,);
-        else if(n >= 5)
-             count += 1+getChange(n-5);
-        else if(n >= 1)
-             count += 1+getChange(n-1);
-        else
-             return 0;
+        if(denom == 25){
+            nextDenom = 10;
+            break;
+        }
+        if(denom == 10){
+            nextDenom = 5;
+            break;
+        }
+        if(denom == 5){
+            nextDenom = 1;
+            break;
+        }
+        if(denom == 1){
+            return 1;
+        }
+        
+        for(int i = 0; i*denom <= n;++i){
+            count += (n-i*denom,nextDenom);
+        }
         return count;
+        
+    }
             
     public static void main(String[] args){
-        System.out.println(getChange(100));
+        System.out.println(getChange(100,25));
     
     
     }
     
-    }
-
+    
 
 }
