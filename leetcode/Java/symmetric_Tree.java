8b68a902a07ab1cc4447d248ce03abfc2e78a41a
jckd1122 <wmiao1122@gmail.com>
2012-10-13 03:34:28 -0300

    Update leetcode/Java/symmetric_Tree.java

--- leetcode/Java/symmetric_Tree.java
+++ leetcode/Java/symmetric_Tree.java
@@ -5,11 +5,11 @@ public class Solution {
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         if(root.left == null && root.right == null)
             return true;
-        if((root.left == null && root.right != null)|| (root.left != null && root.right == null))
+        if((root.left == null)^(root.right == null))
             return false;
-            
         queue.offer(root.left);
         queue.offer(root.right);
+        
         while(!queue.isEmpty()){
             TreeNode node1 = queue.poll();
             TreeNode node2 = queue.poll();
@@ -20,20 +20,17 @@ public class Solution {
                 queue.offer(node1.left);
                 queue.offer(node2.right);
             }
-            else{
-                if((node1.left == null && node2.right != null)|| (node1.left != null && node2.right == null))
-                    return false;
-            }
+            if((node1.left == null)^(node2.right == null))
+                return false;
+            
             
             if(node1.right != null && node2.left != null){
                 queue.offer(node1.right);
                 queue.offer(node2.left);
             }
-             else{
-                if((node1.right == null && node2.left != null)|| (node1.right != null && node2.left == null))
-                    return false;
-            }
-
+            if((node1.right == null)^(node2.left == null))
+                return false;
+ 
         }
         return true;
     }
