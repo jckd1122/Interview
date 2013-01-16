224ff2ff8eeddcbc048c0b230453f13a4b882971
jckd1122 <wmiao1122@gmail.com>
2013-01-16 15:56:45 -0500

    Update leetcode_redo/C++/validParentheses.cpp

--- leetcode_redo/C++/validParentheses.cpp
+++ leetcode_redo/C++/validParentheses.cpp
@@ -1,1 +1,34 @@
-i
+class Solution {
+public:
+    bool isValid(string s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<int> v;
+        
+        
+        for(int i = 0; i < s.length();++i){
+            if(s[i] == '(')
+                v.push_back(s[i]);
+            if(s[i] == '[')
+                v.push_back(s[i]);
+            if(s[i] == '{')
+                v.push_back(s[i]);
+            if(s[i] == ')'){
+                if(v[v.size()-1] != '('
+                    return false;
+                a.pop_back();
+            }
+            if(s[i] == ']'){
+                if(v[v.size()-1] != '['
+                    return false;
+                a.pop_back();
+            }
+            if(s[i] == '}'){
+                if(v[v.size()-1] != '{'
+                    return false;
+                a.pop_back();
+            }
+        }
+        return (a.size() == 0 && b.size() == 0 )&& (c.size() == 0);
+    }
+};
